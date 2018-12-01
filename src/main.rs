4b230ebc27f09e2c887ac6e9fd20f0384b0f77b8
Chris Butler <chrisbutler296@gmail.com>
2018-12-01 01:10:51 -0500

    Day01-part2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     fs,
     io,
     io::BufRead,
@@ -29,16 +30,28 @@ fn run1() -> Result<(), failure::Error> {
 }
 
 fn run2() -> Result<(), failure::Error> {
-    let file = fs::File::open("input-1.txt")?;
+    let file = fs::File::open("input-2.txt")?;
     let input = io::BufReader::new(file);
 
-    let mut freqs: Vec<i32> = vec![];
+    let mut changes: Vec<i32> = vec![];
     for line in input.lines() {
         let line = line?;
-        freqs.push(line.parse()?);
+        changes.push(line.parse()?);
     }
-    let freq_change: i32 = freqs.iter().sum();
-    println!("{}", freq_change);
+
+    let mut seen = HashSet::new();
+    let mut freq = 0;
+    seen.insert(freq);
+
+    for change in changes.iter().cycle() {
+        freq += change;
+
+        if seen.contains(&freq) {
+            break;
+        }
+        seen.insert(freq);
+    }
+    println!("{}", freq);
 
     Ok(())
 }
