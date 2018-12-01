5b8abc514e835bf738dc0ce548532ba509ee6743
Chris Butler <chrisbutler296@gmail.com>
2018-12-01 01:04:27 -0500

    Filler

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,28 @@ use std::{
 use failure;
 
 fn main() {
-    match run() {
+    match run2() {
         Ok(()) => {},
         Err(ref err) => eprintln!("{:?}", err),
     }
 }
 
-fn run() -> Result<(), failure::Error> {
+fn run1() -> Result<(), failure::Error> {
+    let file = fs::File::open("input-1.txt")?;
+    let input = io::BufReader::new(file);
+
+    let mut freqs: Vec<i32> = vec![];
+    for line in input.lines() {
+        let line = line?;
+        freqs.push(line.parse()?);
+    }
+    let freq_change: i32 = freqs.iter().sum();
+    println!("{}", freq_change);
+
+    Ok(())
+}
+
+fn run2() -> Result<(), failure::Error> {
     let file = fs::File::open("input-1.txt")?;
     let input = io::BufReader::new(file);
 
