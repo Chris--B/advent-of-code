b4c1ca9cb6389569b501357cb2f955c5aa40bdad
Chris Butler <chrisbutler296@gmail.com>
2018-12-01 01:02:46 -0500

    Day01-part1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{
+    fs,
+    io,
+    io::BufRead,
+};
+
+use failure;
+
 fn main() {
-    println!("Hello, world!");
+    match run() {
+        Ok(()) => {},
+        Err(ref err) => eprintln!("{:?}", err),
+    }
+}
+
+fn run() -> Result<(), failure::Error> {
+    let file = fs::File::open("input-1.txt")?;
+    let input = io::BufReader::new(file);
+
+    let mut freqs: Vec<i32> = vec![];
+    for line in input.lines() {
+        let line = line?;
+        freqs.push(line.parse()?);
+    }
+    let freq_change: i32 = freqs.iter().sum();
+    println!("{}", freq_change);
+
+    Ok(())
 }
