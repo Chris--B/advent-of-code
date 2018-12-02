7e5d90943e6dc65d9df98cc196064b4085ceae92
Chris Butler <chrisbutler296@gmail.com>
2018-12-02 01:13:20 -0500

    env parse

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,20 @@ use std::fs;
 use std::io;
 use std::io::BufRead;
 use itertools::Itertools;
+use std::env;
 
 fn main() {
-    match run2() {
-        Ok(()) => {},
-        Err(ref err) => eprintln!("{:?}", err),
+    let run = env::args().nth(1).unwrap_or("1".to_string());
+    if run == "1" {
+        match run1() {
+            Ok(()) => {},
+            Err(ref err) => eprintln!("{:?}", err),
+        }
+    } else if run == "2" {
+        match run2() {
+            Ok(()) => {},
+            Err(ref err) => eprintln!("{:?}", err),
+        }
     }
 }
 
