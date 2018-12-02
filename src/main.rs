17647cf9fccd27e266380ac93f8274c4301b251b
Chris Butler <chrisbutler296@gmail.com>
2018-12-02 01:06:34 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+#![allow(dead_code)]
 
 use std::collections::HashMap;
 use std::fs;
 use std::io;
 use std::io::BufRead;
+use itertools::Itertools;
 
 fn main() {
     match run2() {
@@ -33,27 +35,36 @@ fn run2() -> Result<(), failure::Error> {
     let file = fs::File::open("input-01.txt")?;
     let input = io::BufReader::new(file);
 
-    let mut box_ids = vec![];
-    for line in input.lines() {
-        let box_id = line?;
-        box_ids.push(box_id);
-    }
+    // Read each line as a String.
+    // e.g. "oeylbtcxjqnzhgkyylfapviusr"
+    let box_ids: Vec<String> = input
+        .lines()
+        .map(|line| line.map_err(|e| e.into()))
+        .collect::<Result<_, failure::Error>>()?;
 
-    let mut pairs = vec![];
-    for a in box_ids.iter() {
-        for b in box_ids.iter() {
-            if a != b {
-                pairs.push((a, b));
-            }
-        }
-    }
+    // Create all pairs of all lines, and then filter them
+    let ids = box_ids.iter().map(String::as_str);
+    let pairs: Vec<_> = ids.clone()
+        .cartesian_product(ids)
+        // The "<" here removes duplicate pairs
+        // since (a, b) == (b, a), for our problem
+        .filter(|p| p.0 != p.1 && p.0 < p.1)
+        .filter(|p| strsim::hamming(p.0, p.1).unwrap() == 1)
+        .collect();
+    // We should now only have 1 pair.
+    let pair = pairs.first().unwrap();
+    assert_eq!(pairs.len(), 1);
 
-    pairs.sort_unstable_by_key(|p| {
-        let (a, b) = p;
-        strsim::hamming(a, b).unwrap()
-    });
-
-    println!("{:?}", pairs[0]);
+    // Combine characters that appear in both ids.
+    let result = String::from_utf8(
+        pair.0.chars().zip(pair.1.chars())
+        .filter_map(|p| {
+            if p.0 == p.1 { Some(p.0 as u8)} else { None }
+        })
+        .collect::<Vec<u8>>()
+    ).unwrap();
+    assert_eq!(pair.0.len(), result.len()+1);
+    println!("{}", result);
 
     Ok(())
 }
@@ -66,8 +77,8 @@ fn checksum_id(box_id: &str) -> (bool, bool) {
         *count += 1;
     }
 
-    let mut twos   = counts.values().find(|count| **count == 2).is_some();
-    let mut threes = counts.values().find(|count| **count == 3).is_some();
+    let twos   = counts.values().find(|count| **count == 2).is_some();
+    let threes = counts.values().find(|count| **count == 3).is_some();
     (twos, threes)
 }
 
