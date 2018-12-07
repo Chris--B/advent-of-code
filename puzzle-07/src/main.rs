ad1be8591d186d51196c34ff90f7aecee2ab0b19
Chris Butler <chrisbutler296@gmail.com>
2018-12-06 23:49:04 -0500

    Empty day 7

---
+++ puzzle-07/src/main.rs
@@ -0,0 +1,39 @@
+
+use std::{
+    collections::*,
+    env,
+    fs,
+    io::{
+        self,
+        BufRead,
+    },
+};
+
+fn main() {
+    let run = env::args().nth(1).unwrap_or("1".to_string());
+    if run == "1" {
+        match run1() {
+            Ok(()) => {},
+            Err(ref err) => eprintln!("{:?}", err),
+        }
+    } else if run == "2" {
+        match run2() {
+            Ok(()) => {},
+            Err(ref err) => eprintln!("{:?}", err),
+        }
+    }
+}
+
+fn run1() -> Result<(), failure::Error> {
+    let file = fs::File::open("input.txt")?;
+    let input = io::BufReader::new(file);
+
+    Ok(())
+}
+
+fn run2() -> Result<(), failure::Error> {
+    let file = fs::File::open("input.txt")?;
+    let input = io::BufReader::new(file);
+
+    Ok(())
+}
