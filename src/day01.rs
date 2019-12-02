cbae5cc8f37b8607b50cc500ee56cbdabf84bc77
Chris Butler <chrisbutler296@gmail.com>
2019-12-02 13:47:06 -0500

    Solution for 01.01

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,14 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+pub fn parse_masses(input: &str) -> Vec<u32> {
+    input
+        .split_whitespace()
+        .map(|line| line.trim().parse::<u32>().unwrap())
+        .collect()
+}
+
 #[aoc(day1, part1)]
-pub fn simple(input: &str) -> u32 {
-    0
+pub fn simple(input: &[u32]) -> u32 {
+    input.iter().map(|m| m / 3 - 2).sum()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-extern crate aoc_runner;
-#[macro_use]
-extern crate aoc_runner_derive;
+use aoc_runner_derive::aoc_lib;
 
-pub mod day01;
+mod day01;
 
 aoc_lib! { year = 2019 }
