a63dc3b45b061f78012c0784926e06228f5a2ccc
Chris Butler <chrisbutler296@gmail.com>
2019-12-10 00:02:54 -0500

    Add In/Out instructions

--- intcode/src/opcodes.rs
+++ intcode/src/opcodes.rs
@@ -1,15 +1,27 @@
-#[derive(Copy, Clone, Debug)]
+use crate::vm::Atom;
+
+/// Each operation the Vm can execute
+///
+/// r# represents argument #
+/// [r#] represents the argument evalutated according to the parameter mode specified
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Opcode {
     /// [r3] = [r1] + [r2]
     Add = 1,
     /// [r3] = [r1] * [r2]
     Mul = 2,
 
+    /// [r1] = <user-input>
+    In = 3,
+
+    /// <user-output> from [r1]
+    Out = 4,
+
     /// Stop executing the vm
     Hlt = 99,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum ParamMode {
     /// The parameter represents an address
     Addr,
@@ -18,7 +30,7 @@ pub enum ParamMode {
 }
 
 impl ParamMode {
-    pub fn from_digit(digit: u8) -> Option<ParamMode> {
+    pub fn from_digit(digit: Atom) -> Option<ParamMode> {
         match digit {
             0 => Some(ParamMode::Addr),
             1 => Some(ParamMode::Imm),
@@ -28,10 +40,13 @@ impl ParamMode {
 }
 
 impl Opcode {
-    pub fn from_digits(digits: u8) -> Option<Opcode> {
+    pub fn from_digits(digits: Atom) -> Option<Opcode> {
         match digits {
             1 => Some(Opcode::Add),
             2 => Some(Opcode::Mul),
+
+            3 => Some(Opcode::In),
+            4 => Some(Opcode::Out),
 
             99 => Some(Opcode::Hlt),
             _ => None,
--- intcode/src/vm.rs
+++ intcode/src/vm.rs
@@ -4,10 +4,9 @@
 //! programs
 
 use smallvec::SmallVec;
+
 use std::fmt;
 use std::fmt::Write;
-
-use crate::opcodes;
 
 /// Individual entries in the VM's memory are represented as "atom" types
 /// This can be a signed or unsigned integer of unspecified size.
@@ -245,60 +244,190 @@ impl Vm {
     ///
     /// Returns Ok(self.ip()) if the vm executes `HALT`, otherwise Err() describes what happened.
     pub fn run(&mut self) -> Result<usize, VmStopReason> {
+        use crate::opcodes::{Opcode, ParamMode};
+
         loop {
+            self.ticks += 1;
+
             let ip_atom = self.read_atom(self.ip)?;
+            let opcode = Opcode::from_digits(ip_atom % 100);
 
-            let header_raw = (ip_atom % 100) as u8;
-            let header = opcodes::Opcode::from_digits(header_raw);
+            // Instruction decoding can iteratively "strip" away values from this as it parses
+            // the packed data - e.g. param modes.
+            let mut ip_atom_num = ip_atom / 100;
 
-            match header {
-                Some(opcodes::Opcode::Add) => {
-                    // Extract addresses of inputs
-                    let a_arg0 = self.read_atom(self.ip + 1)?;
-                    let a_arg1 = self.read_atom(self.ip + 2)?;
-
+            match opcode {
+                Some(Opcode::Add) => {
                     // Fetch input values
-                    let arg0 = self.read_atom(a_arg0 as usize)?;
-                    let arg1 = self.read_atom(a_arg1 as usize)?;
+                    let args: (Atom, Atom) = {
+                        // Extract param values
+                        let a0 = {
+                            let p0 = self.read_atom(self.ip + 1)?;
+                            let m0 = ParamMode::from_digit(ip_atom_num % 10).unwrap();
+                            ip_atom_num /= 10;
 
-                    // Extract output location
+                            match m0 {
+                                // Fetch value from memory
+                                ParamMode::Addr => self.read_atom(p0 as usize)?,
+
+                                // Use immediate value
+                                ParamMode::Imm => p0,
+                            }
+                        };
+
+                        let a1 = {
+                            let p1 = self.read_atom(self.ip + 2)?;
+                            let m1 = ParamMode::from_digit(ip_atom_num % 10).unwrap();
+                            ip_atom_num /= 10;
+
+                            match m1 {
+                                // Fetch value from memory
+                                ParamMode::Addr => self.read_atom(p1 as usize)?,
+
+                                // Use immediate value
+                                ParamMode::Imm => p1,
+                            }
+                        };
+
+                        (a0, a1)
+                    };
+
+                    // Verify that the output ParamMode is valid
+                    #[cfg(debug_assertions)]
+                    {
+                        let mode = ParamMode::from_digit(ip_atom_num % 10).unwrap();
+                        ip_atom_num /= 10;
+                        if mode == ParamMode::Imm {
+                            return Err(VmStopReason::IllegalInstruction {
+                                ip: self.ip,
+                                what: format!(
+                                    "Opcode {:?} at {} used illegal \"Imm\" mode on writeout param",
+                                    opcode, self.ip
+                                ),
+                            });
+                        }
+                    }
+                    // Fetch output address
                     let a_out = self.read_atom(self.ip + 3)?;
 
                     // Write back result
-                    self.write_atom(a_out as usize, arg0 + arg1)?;
+                    self.write_atom(a_out as usize, args.0 + args.1)?;
 
                     self.ip += 4;
-                    continue;
                 }
-                Some(opcodes::Opcode::Mul) => {
-                    // Extract addresses of inputs
-                    let a_arg0 = self.read_atom(self.ip + 1)?;
-                    let a_arg1 = self.read_atom(self.ip + 2)?;
-
+                Some(Opcode::Mul) => {
                     // Fetch input values
-                    let arg0 = self.read_atom(a_arg0 as usize)?;
-                    let arg1 = self.read_atom(a_arg1 as usize)?;
+                    let args: (Atom, Atom) = {
+                        // Extract param values
+                        let a0 = {
+                            let p0 = self.read_atom(self.ip + 1)?;
+                            let m0 = ParamMode::from_digit(ip_atom_num % 10).unwrap();
+                            ip_atom_num /= 10;
 
-                    // Extract output location
+                            match m0 {
+                                // Fetch value from memory
+                                ParamMode::Addr => self.read_atom(p0 as usize)?,
+
+                                // Use immediate value
+                                ParamMode::Imm => p0,
+                            }
+                        };
+
+                        let a1 = {
+                            let p1 = self.read_atom(self.ip + 2)?;
+                            let m1 = ParamMode::from_digit(ip_atom_num % 10).unwrap();
+                            ip_atom_num /= 10;
+
+                            match m1 {
+                                // Fetch value from memory
+                                ParamMode::Addr => self.read_atom(p1 as usize)?,
+
+                                // Use immediate value
+                                ParamMode::Imm => p1,
+                            }
+                        };
+
+                        (a0, a1)
+                    };
+
+                    // Verify that the output ParamMode is valid
+                    #[cfg(debug_assertions)]
+                    {
+                        let mode = ParamMode::from_digit(ip_atom_num % 10).unwrap();
+                        ip_atom_num /= 10;
+                        if mode == ParamMode::Imm {
+                            return Err(VmStopReason::IllegalInstruction {
+                                ip: self.ip,
+                                what: format!(
+                                    "Opcode {:?} at {} used illegal \"Imm\" mode on writeout param",
+                                    opcode, self.ip
+                                ),
+                            });
+                        }
+                    }
+                    // Fetch output address
                     let a_out = self.read_atom(self.ip + 3)?;
 
                     // Write back result
-                    self.write_atom(a_out as usize, arg0 * arg1)?;
+                    self.write_atom(a_out as usize, args.0 * args.1)?;
 
                     self.ip += 4;
-                    continue;
                 }
-                Some(opcodes::Opcode::Hlt) => return Ok(self.ip),
+                Some(Opcode::In) => {
+                    let a_out = self.read_atom(self.ip + 1)? as usize;
+
+                    if self.input_buffer.is_empty() {
+                        return Err(VmStopReason::BlockedOnInput { ip: self.ip });
+                    }
+
+                    // Fetch the next input value
+                    let value = self.input_buffer[0];
+                    self.input_buffer.remove(0);
+
+                    // and write it to memory
+                    self.write_atom(a_out, value)?;
+
+                    self.ip += 2;
+                }
+
+                Some(Opcode::Out) => {
+                    // Fetch value to output
+                    let a0 = {
+                        let p0 = self.read_atom(self.ip + 1)?;
+                        let m0 = ParamMode::from_digit(ip_atom_num % 10).unwrap();
+                        ip_atom_num /= 10;
+
+                        match m0 {
+                            // Fetch value from memory
+                            ParamMode::Addr => self.read_atom(p0 as usize)?,
+
+                            // Use immediate value
+                            ParamMode::Imm => p0,
+                        }
+                    };
+
+                    // Write to "output"
+                    self.output_buffer.push(a0);
+
+                    self.ip += 2;
+                }
+                Some(Opcode::Hlt) => return Ok(self.ip),
                 _ => {
                     return Err(VmStopReason::UnknownInstruction {
                         ip: self.ip,
                         what: format!(
-                            "Unrecognized opcode header at address {}: {}",
-                            self.ip, header_raw
+                            "Unrecognized opcode header at address {}: {:?}",
+                            self.ip, opcode
                         ),
                     })
                 }
             }
+
+            assert_eq!(
+                ip_atom_num, 0,
+                "opcode {:?} didn't use all of its param modes",
+                opcode
+            );
         }
     }
 }
@@ -418,4 +547,100 @@ mod day_02 {
 }
 
 #[cfg(test)]
-mod day_05 {}
+mod day_05 {
+    use super::*;
+
+    #[test]
+    fn check_echo() {
+        for input in 100..=110 {
+            let intcode = vec![3, 0, 4, 0, 99];
+            let mut vm = Vm::with_memory(intcode);
+
+            let why = vm.run();
+            assert_eq!(
+                why,
+                Err(VmStopReason::BlockedOnInput { ip: 0 }),
+                "vm didn't stop to wait on input"
+            );
+
+            vm.add_input(input);
+
+            let why = vm.run();
+            assert_eq!(why, Ok(4), "vm finished executing at an unexpected address");
+            assert_eq!(
+                vm.get_output(),
+                &[input],
+                "vm output didn't match expected output ({})",
+                input
+            );
+        }
+    }
+
+    #[test]
+    fn check_part1_input() {
+        let mut intcode = vec![
+            3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 101, 20, 183, 224, 101, -63, 224, 224,
+            4, 224, 1002, 223, 8, 223, 101, 6, 224, 224, 1, 223, 224, 223, 1101, 48, 40, 225, 1101,
+            15, 74, 225, 2, 191, 40, 224, 1001, 224, -5624, 224, 4, 224, 1002, 223, 8, 223, 1001,
+            224, 2, 224, 1, 223, 224, 223, 1101, 62, 60, 225, 1102, 92, 15, 225, 102, 59, 70, 224,
+            101, -885, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 7, 224, 224, 1, 224, 223, 223, 1,
+            35, 188, 224, 1001, 224, -84, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 2, 224, 1, 223,
+            224, 223, 1001, 66, 5, 224, 1001, 224, -65, 224, 4, 224, 102, 8, 223, 223, 1001, 224,
+            3, 224, 1, 223, 224, 223, 1002, 218, 74, 224, 101, -2960, 224, 224, 4, 224, 1002, 223,
+            8, 223, 1001, 224, 2, 224, 1, 224, 223, 223, 1101, 49, 55, 224, 1001, 224, -104, 224,
+            4, 224, 102, 8, 223, 223, 1001, 224, 6, 224, 1, 224, 223, 223, 1102, 43, 46, 225, 1102,
+            7, 36, 225, 1102, 76, 30, 225, 1102, 24, 75, 224, 101, -1800, 224, 224, 4, 224, 102, 8,
+            223, 223, 101, 2, 224, 224, 1, 224, 223, 223, 1101, 43, 40, 225, 4, 223, 99, 0, 0, 0,
+            677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999,
+            1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105,
+            1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999,
+            1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1,
+            99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 1008, 226, 226,
+            224, 1002, 223, 2, 223, 1005, 224, 329, 1001, 223, 1, 223, 8, 226, 677, 224, 102, 2,
+            223, 223, 1006, 224, 344, 1001, 223, 1, 223, 1007, 226, 677, 224, 1002, 223, 2, 223,
+            1005, 224, 359, 101, 1, 223, 223, 1008, 677, 226, 224, 102, 2, 223, 223, 1006, 224,
+            374, 1001, 223, 1, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 389, 1001,
+            223, 1, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 404, 101, 1, 223, 223,
+            1007, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 419, 101, 1, 223, 223, 7, 677, 226,
+            224, 1002, 223, 2, 223, 1005, 224, 434, 1001, 223, 1, 223, 1007, 677, 677, 224, 1002,
+            223, 2, 223, 1006, 224, 449, 101, 1, 223, 223, 107, 226, 226, 224, 1002, 223, 2, 223,
+            1006, 224, 464, 1001, 223, 1, 223, 1108, 677, 677, 224, 1002, 223, 2, 223, 1005, 224,
+            479, 101, 1, 223, 223, 8, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 494, 101, 1,
+            223, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 509, 1001, 223, 1, 223, 1107,
+            677, 226, 224, 102, 2, 223, 223, 1005, 224, 524, 1001, 223, 1, 223, 1108, 677, 226,
+            224, 1002, 223, 2, 223, 1005, 224, 539, 1001, 223, 1, 223, 1108, 226, 677, 224, 102, 2,
+            223, 223, 1006, 224, 554, 101, 1, 223, 223, 108, 226, 677, 224, 102, 2, 223, 223, 1005,
+            224, 569, 1001, 223, 1, 223, 8, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 584, 101,
+            1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 599, 1001, 223, 1, 223,
+            108, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 614, 101, 1, 223, 223, 1008, 677, 677,
+            224, 102, 2, 223, 223, 1006, 224, 629, 1001, 223, 1, 223, 107, 226, 677, 224, 102, 2,
+            223, 223, 1006, 224, 644, 101, 1, 223, 223, 1107, 677, 677, 224, 1002, 223, 2, 223,
+            1005, 224, 659, 1001, 223, 1, 223, 7, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 674,
+            101, 1, 223, 223, 4, 223, 99, 226,
+        ];
+
+        let mut vm = Vm::with_memory(intcode);
+
+        let why = vm.run();
+        assert_eq!(
+            why,
+            Err(VmStopReason::BlockedOnInput { ip: 0 }),
+            "vm didn't stop to wait on input"
+        );
+
+        vm.add_input(1);
+
+        let why = vm.run();
+        assert_eq!(
+            why,
+            Ok(222),
+            "vm finished executing at an unexpected address"
+        );
+
+        assert_eq!(
+            vm.get_output(),
+            &[0, 0, 0, 0, 0, 0, 0, 0, 0, 13346482],
+            "vm output didn't match expected output"
+        );
+    }
+}
