17f9aff2b2f0fd1b2d961ce6be7683179cb7ea45
Chris Butler <chrisbutler296@gmail.com>
2021-11-30 22:15:47 -0700

    2021/day1

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,9 +1,7 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-type Foo = usize;
-
 #[aoc_generator(day1)]
-pub fn parse_input(input: &str) -> Vec<Foo> {
+pub fn parse_input(input: &str) -> Vec<u32> {
     input
         .lines()
         .map(|line| line.trim().parse().unwrap())
@@ -12,12 +10,32 @@ pub fn parse_input(input: &str) -> Vec<Foo> {
 
 // Part1 ======================================================================
 #[aoc(day1, part1)]
-pub fn part1(_input: &[Foo]) -> Foo {
-    unimplemented!();
+pub fn part1(input: &[u32]) -> u32 {
+    let mut count = 0;
+
+    for window in input.windows(2) {
+        if window[0] < window[1] {
+            count += 1;
+        }
+    }
+
+    count
 }
 
 // Part2 ======================================================================
-// #[aoc(day1, part2)]
-pub fn part2(_input: &[Foo]) -> Foo {
-    unimplemented!();
+#[aoc(day1, part2)]
+pub fn part2(input: &[u32]) -> u32 {
+    let mut last_sum = u32::MAX;
+    let mut count = 0;
+
+    for window in input.windows(3) {
+        let s = window.iter().sum();
+        if last_sum < s {
+            count += 1;
+        }
+
+        last_sum = s;
+    }
+
+    count
 }
