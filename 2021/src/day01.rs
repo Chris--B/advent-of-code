73af0435f7af255b9ab0cabb9c884a11a7ea1c39
Chris Butler <chrisbutler296@gmail.com>
2021-12-01 21:23:17 -0700

    Add more day1 solutions

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,4 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
 #[aoc_generator(day1)]
 pub fn parse_input(input: &str) -> Vec<u32> {
@@ -9,7 +10,8 @@ pub fn parse_input(input: &str) -> Vec<u32> {
 }
 
 // Part1 ======================================================================
-#[aoc(day1, part1)]
+#[aoc(day1, part1, simple_loop)]
+#[inline(never)]
 pub fn part1(input: &[u32]) -> u32 {
     let mut count = 0;
 
@@ -22,8 +24,15 @@ pub fn part1(input: &[u32]) -> u32 {
     count
 }
 
+#[aoc(day1, part1, tuple_windows)]
+#[inline(never)]
+pub fn part1_itertools(input: &[u32]) -> u32 {
+    input.iter().tuple_windows().filter(|(a, b)| a < b).count() as u32
+}
+
 // Part2 ======================================================================
-#[aoc(day1, part2)]
+#[aoc(day1, part2, simple_loop)]
+#[inline(never)]
 pub fn part2(input: &[u32]) -> u32 {
     let mut last_sum = u32::MAX;
     let mut count = 0;
@@ -38,4 +47,28 @@ pub fn part2(input: &[u32]) -> u32 {
     }
 
     count
+}
+
+#[aoc(day1, part2, clever_loop)]
+#[inline(never)]
+pub fn part2_clever_loop(input: &[u32]) -> u32 {
+    let mut count = 0;
+
+    for window in input.windows(4) {
+        if window[0] < window[3] {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+#[aoc(day1, part2, tuple_windows)]
+#[inline(never)]
+pub fn part2_itertools(input: &[u32]) -> u32 {
+    input
+        .iter()
+        .tuple_windows()
+        .filter(|(a, _b, _c, d)| a < d)
+        .count() as u32
 }
