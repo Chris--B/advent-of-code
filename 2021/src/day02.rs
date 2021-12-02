5aff8a46c351d512fb007aaf5b8d595da4cc4a78
Chris Butler <chrisbutler296@gmail.com>
2021-12-01 23:19:13 -0700

    Tweak Day2 to use FromStr

--- 2021/src/day02.rs
+++ 2021/src/day02.rs
@@ -1,43 +1,54 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
+use std::str::FromStr;
+
 #[derive(Copy, Clone, Debug)]
 pub enum Cmd {
-    Forward,
-    Down,
-    Up,
+    Forward(u64),
+    Down(u64),
+    Up(u64),
 }
 use Cmd::*;
 
+impl FromStr for Cmd {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, ()> {
+        let (dir, spd) = s.split_once(' ').unwrap();
+        let spd = spd.trim().parse().unwrap();
+        let cmd = match dir {
+            "forward" => Forward(spd),
+            "down" => Down(spd),
+            "up" => Up(spd),
+            _ => panic!("Unrecognized direction"),
+        };
+
+        Ok(cmd)
+    }
+}
+
 #[aoc_generator(day2)]
-pub fn parse_input(input: &str) -> Vec<(Cmd, u64)> {
+pub fn parse_input(input: &str) -> Vec<Cmd> {
     input
         .lines()
-        .map(|line| {
-            let mut iter = line.trim().split(' ');
-            let dir = iter.next().unwrap();
-            let amount = iter.next().unwrap();
-
-            match (dir, amount) {
-                ("forward", x) => (Forward, x.parse().unwrap()),
-                ("down", x) => (Down, x.parse().unwrap()),
-                ("up", x) => (Up, x.parse().unwrap()),
-                _ => panic!(),
-            }
-        })
-        .collect()
+        .map(str::parse)
+        .collect::<Result<_, ()>>()
+        .unwrap()
 }
 
 // Part1 ======================================================================
 #[aoc(day2, part1)]
 #[inline(never)]
-pub fn part1(input: &[(Cmd, u64)]) -> u64 {
+pub fn part1(input: &[Cmd]) -> u64 {
+    // x is horizontal distance
+    // y is DEPTH, and is kind of backwards (except for graphics programmers)
     let (mut x, mut y) = (0, 0);
 
-    for (cmd, spd) in input {
+    for cmd in input {
         match cmd {
-            Forward => x += spd,
-            Down => y += spd,
-            Up => y -= spd,
+            Forward(spd) => x += spd,
+            Down(spd) => y += spd,
+            Up(spd) => y -= spd,
         }
     }
 
@@ -47,24 +58,20 @@ pub fn part1(input: &[(Cmd, u64)]) -> u64 {
 // Part2 ======================================================================
 #[aoc(day2, part2)]
 #[inline(never)]
-pub fn part2(input: &[(Cmd, u64)]) -> u64 {
+pub fn part2(input: &[Cmd]) -> u64 {
+    // x is horizontal distance
+    // y is DEPTH
     let (mut x, mut y) = (0, 0);
     let mut aim = 0;
 
-    for (cmd, spd) in input {
+    for cmd in input {
         match cmd {
-            Down => aim += spd,
-            Up => aim -= spd,
-            /*
-            forward X does two things:
-
-            It increases your horizontal position by X units.
-            It increases your depth by your aim multiplied by X.
-            */
-            Forward => {
+            Forward(spd) => {
                 x += spd;
                 y += aim * spd;
             }
+            Down(spd) => aim += spd,
+            Up(spd) => aim -= spd,
         }
     }
 
