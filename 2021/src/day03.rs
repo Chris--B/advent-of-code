07abb66d3d69c6a7599e842514221b15a3d0ce01
Chris Butler <chrisbutler296@gmail.com>
2021-12-02 23:33:02 -0700

    Minimal cleanup

--- 2021/src/day03.rs
+++ 2021/src/day03.rs
@@ -1,11 +1,11 @@
 use aoc_runner_derive::aoc;
 
-const BITS: usize = 12;
-
 // Part1 ======================================================================
 #[aoc(day3, part1)]
 #[inline(never)]
 pub fn part1(input: &str) -> u64 {
+    const BITS: usize = 12;
+
     // count bits at each position
     let mut count_of_ones = [0; BITS];
     let count_of_bits = input.lines().count();
@@ -19,7 +19,7 @@ pub fn part1(input: &str) -> u64 {
         }
     }
 
-    let mut gamma = 0;
+    let mut gamma: u64 = 0;
     for count in count_of_ones {
         gamma <<= 1;
         if count >= count_of_bits / 2 {
@@ -27,7 +27,8 @@ pub fn part1(input: &str) -> u64 {
         }
     }
 
-    let epsilon = !gamma & 0b1111_1111_1111;
+    let mask = u64::MAX >> gamma.leading_zeros();
+    let epsilon = !gamma & mask;
 
     gamma * epsilon
 }
@@ -36,41 +37,12 @@ pub fn part1(input: &str) -> u64 {
 #[aoc(day3, part2)]
 #[inline(never)]
 pub fn part2(input: &str) -> u64 {
-    let mut gamma: u64;
-    let epsilon: u64;
-
-    {
-        // count bits at each position
-        let mut count_of_ones = [0; BITS];
-        let count_of_bits = input.lines().count();
-
-        for (idx, count) in count_of_ones.iter_mut().enumerate() {
-            for line in input.lines() {
-                let bits = line.as_bytes();
-                if bits[idx] == b'1' {
-                    *count += 1;
-                }
-            }
-        }
-
-        gamma = 0;
-        for count in count_of_ones {
-            gamma <<= 1;
-            if count >= count_of_bits / 2 {
-                gamma |= 1;
-            }
-        }
-
-        epsilon = !gamma & 0b1111_1111_1111;
-    }
-
     let o2_reading;
     {
         let mut lines: Vec<_> = input.lines().collect();
+        let bits_per_line = lines[0].len();
 
-        'bit_loop: for bit_idx in 0..BITS {
-            eprintln!();
-
+        'bit_loop: for bit_idx in 0..bits_per_line {
             let mut count_of_ones = 0;
             let count_of_bits = lines.len();
 
@@ -80,9 +52,6 @@ pub fn part2(input: &str) -> u64 {
                     count_of_ones += 1;
                 }
             }
-
-            // dbg!(bit_idx, count_of_ones);
-            // dbg!(&lines);
 
             for line_idx in (0..lines.len()).rev() {
                 let bits = lines[line_idx].as_bytes();
@@ -94,13 +63,9 @@ pub fn part2(input: &str) -> u64 {
                 };
 
                 if should_cull {
-                    // dbg!(lines[line_idx], bit_idx, count_of_ones, count_of_bits / 2);
-
                     let removed = lines[line_idx];
-                    // dbg!(removed);
 
                     lines.swap_remove(line_idx);
-                    // dbg!(&lines);
 
                     if lines.len() == 1 {
                         break 'bit_loop;
@@ -110,15 +75,16 @@ pub fn part2(input: &str) -> u64 {
         }
 
         assert!(lines.len() == 1);
-        // dbg!(lines[0]);
+
         o2_reading = u64::from_str_radix(lines[0], 2).unwrap();
     }
 
     let co_reading;
     {
         let mut lines: Vec<_> = input.lines().collect();
+        let bits_per_line = lines[0].len();
 
-        for bit_idx in 0..BITS {
+        for bit_idx in 0..bits_per_line {
             let mut count_of_ones = 0;
             let count_of_bits = lines.len();
 
@@ -152,28 +118,23 @@ pub fn part2(input: &str) -> u64 {
         co_reading = u64::from_str_radix(lines[0], 2).unwrap();
     }
 
-    // dbg!(gamma);
-    // dbg!(epsilon);
-    // dbg!(o2_reading);
-    // dbg!(co_reading);
-
     o2_reading * co_reading
 }
 
-// #[test]
-// fn check_example_2() {
-//     let input = r#"00100
-// 11110
-// 10110
-// 10111
-// 10101
-// 01111
-// 00111
-// 11100
-// 10000
-// 11001
-// 00010
-// 01010"#;
+#[test]
+fn check_example_2() {
+    let input = r#"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"#;
 
-//     assert_eq!(part2(input), 230);
-// }
+    assert_eq!(part2(input), 230);
+}
