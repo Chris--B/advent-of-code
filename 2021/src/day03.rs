d86e537d477bbb095b3a055a0b8dc636da497d35
Chris Butler <chrisbutler296@gmail.com>
2021-12-03 01:21:41 -0700

    Add v2 versions that are cleaner
    These express the algorithm better, and part1's new solution is
    significantly faster.

--- 2021/src/day03.rs
+++ 2021/src/day03.rs
@@ -138,3 +138,170 @@ fn check_example_2() {
 
     assert_eq!(part2(input), 230);
 }
+
+// Helpers ======================================================================
+
+/// Parse given input into structured input
+///
+/// Because the cursed version doesn't use one of these, we can't use `aoc_generator`.
+fn parse_input(input: &str) -> Vec<u32> {
+    input
+        .lines()
+        .map(|line| u32::from_str_radix(line, 2).unwrap())
+        .collect()
+}
+
+/// Count set and unset bits at `bit_idx`
+fn count(readings: &[u32], bit_idx: u32) -> [u32; 2] {
+    let mut counts = [0, 0];
+
+    for x in readings.iter() {
+        let i = (x >> bit_idx) & 0x1;
+        counts[i as usize] += 1;
+    }
+
+    counts
+}
+
+/// Remove all readings where the bit at `bit_idx` does not equal `bit`
+///
+/// This may reorder `readings`!
+fn cull_if_bit_ne(readings: &mut Vec<u32>, bit: u32, bit_idx: u32) {
+    let bit_select = 0x1 << bit_idx;
+    let bit_value = bit << bit_idx;
+
+    // Consider removing each reading
+    // Walk backwards so that removing doesn't disrupt iteration
+    for idx in (0..readings.len()).rev() {
+        if (readings[idx] & bit_select) == bit_value {
+            readings.swap_remove(idx);
+        }
+    }
+}
+
+// Part1 ======================================================================
+#[aoc(day3, part1, v2)]
+#[inline(never)]
+pub fn part1_v2(input: &str) -> u32 {
+    let readings: Vec<_> = parse_input(input);
+
+    let readings_max = *readings.iter().max().unwrap_or(&0);
+    let bits_idx_max = 32 - readings_max.leading_zeros();
+
+    let mut gamma: u32 = 0;
+    for bit_idx in (0..bits_idx_max).rev() {
+        gamma <<= 1;
+
+        let [zeros, ones] = count(&readings, bit_idx);
+
+        if ones >= zeros {
+            gamma |= 1;
+        }
+    }
+
+    let mask = u32::MAX >> gamma.leading_zeros();
+    let epsilon = !gamma & mask;
+
+    gamma * epsilon
+}
+
+#[test]
+fn check_example_1_v2() {
+    let input = r#"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"#;
+
+    assert_eq!(part1_v2(input), 198);
+}
+
+// Part2 ======================================================================
+#[aoc(day3, part2, v2)]
+#[inline(never)]
+pub fn part2_v2(input: &str) -> u32 {
+    let input: Vec<_> = parse_input(input);
+
+    // We can compute these once, since `readings` is only ever shrunk
+    let input_max = *input.iter().max().unwrap_or(&0);
+    let bits_idx_max = 32 - input_max.leading_zeros();
+
+    let oxygen_reading: u32 = {
+        let mut readings = input.clone();
+
+        for bit_idx in (0..bits_idx_max).rev() {
+            // Re-compute this every loop, since we're updating the readings list
+            let [zeros, ones] = count(&readings, bit_idx);
+
+            // NOTE: This changes vs co_reading
+            if ones >= zeros {
+                cull_if_bit_ne(&mut readings, 0x0, bit_idx);
+            } else {
+                cull_if_bit_ne(&mut readings, 0x1, bit_idx);
+            }
+
+            // Once we have a unique solution, stop culling!
+            if readings.len() == 1 {
+                break;
+            }
+        }
+
+        // We absolutely should have filtered everything but 1 by now
+        debug_assert_eq!(readings.len(), 1);
+
+        readings[0]
+    };
+
+    let co_reading: u32 = {
+        let mut readings = input;
+
+        for bit_idx in (0..bits_idx_max).rev() {
+            // Re-compute this every loop, since we're updating the readings list
+            let [zeros, ones] = count(&readings, bit_idx);
+
+            // NOTE: This changes vs oxygen_reading
+            if ones >= zeros {
+                cull_if_bit_ne(&mut readings, 0x1, bit_idx);
+            } else {
+                cull_if_bit_ne(&mut readings, 0x0, bit_idx);
+            }
+
+            // Once we have a unique solution, stop culling!
+            if readings.len() == 1 {
+                break;
+            }
+        }
+
+        // We absolutely should have filtered everything but 1 by now
+        debug_assert_eq!(readings.len(), 1);
+
+        readings[0]
+    };
+
+    oxygen_reading * co_reading
+}
+
+#[test]
+fn check_example_2_v2() {
+    let input = r#"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"#;
+
+    assert_eq!(part2_v2(input), 230);
+}
