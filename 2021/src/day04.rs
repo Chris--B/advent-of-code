4a76a115f77ecbcee3af0912472c837341e78c72
Chris Butler <chrisbutler296@gmail.com>
2021-12-03 23:33:52 -0700

    Mutate the board instead of bools

--- 2021/src/day04.rs
+++ 2021/src/day04.rs
@@ -7,7 +7,7 @@ pub struct BingoState {
 }
 
 #[derive(Clone)]
-pub struct Board([[u32; 5]; 5], [[bool; 5]; 5]);
+pub struct Board([[u32; 5]; 5]);
 
 impl Board {
     fn at(&self, x: u32, y: u32) -> u32 {
@@ -16,31 +16,25 @@ impl Board {
 
     fn draw(&mut self, num: u32) {
         let nums: &mut [u32; 25] = unsafe { std::mem::transmute(&mut self.0) };
-        let marked: &mut [bool; 25] = unsafe { std::mem::transmute(&mut self.1) };
 
-        for (n, m) in nums.iter().zip(marked) {
+        for n in nums {
             if *n == num {
-                *m = true;
+                *n = 0;
             }
         }
-    }
-
-    fn marked(&self, x: u32, y: u32) -> bool {
-        self.1[x as usize][y as usize]
     }
 
     fn score(&self) -> u32 {
         // i do what I want
         let nums: [u32; 25] = unsafe { std::mem::transmute(self.0) };
-        let marked: [bool; 25] = unsafe { std::mem::transmute(self.1) };
 
-        (0..25).filter(|i| !marked[*i]).map(|i| nums[i]).sum()
+        nums.iter().sum()
     }
 
     fn has_won(&self) -> bool {
         // check each row
         for x in 0..5 {
-            let won = (0..5).all(|y| self.marked(x, y));
+            let won = (0..5).all(|y| self.at(x, y) == 0);
 
             if won {
                 return true;
@@ -49,7 +43,7 @@ impl Board {
 
         // check each row
         for y in 0..5 {
-            let won = (0..5).all(|x| self.marked(x, y));
+            let won = (0..5).all(|x| self.at(x, y) == 0);
 
             if won {
                 return true;
@@ -106,16 +100,13 @@ pub fn parse_input(input: &str) -> BingoState {
             .map(|n| n.parse().unwrap())
             .collect();
 
-        let board = Board(
-            [
-                [b0[0], b0[1], b0[2], b0[3], b0[4]],
-                [b1[0], b1[1], b1[2], b1[3], b1[4]],
-                [b2[0], b2[1], b2[2], b2[3], b2[4]],
-                [b3[0], b3[1], b3[2], b3[3], b3[4]],
-                [b4[0], b4[1], b4[2], b4[3], b4[4]],
-            ],
-            Default::default(),
-        );
+        let board = Board([
+            [b0[0], b0[1], b0[2], b0[3], b0[4]],
+            [b1[0], b1[1], b1[2], b1[3], b1[4]],
+            [b2[0], b2[1], b2[2], b2[3], b2[4]],
+            [b3[0], b3[1], b3[2], b3[3], b3[4]],
+            [b4[0], b4[1], b4[2], b4[3], b4[4]],
+        ]);
 
         boards.push(board);
     }
@@ -136,9 +127,6 @@ pub fn part1(input: &BingoState) -> u32 {
             board.draw(n);
 
             if board.has_won() {
-                dbg!(board.score());
-                dbg!(n);
-
                 return board.score() * n;
             }
         }
@@ -161,9 +149,6 @@ pub fn part2(input: &BingoState) -> u32 {
             board.draw(n);
 
             if board.has_won() {
-                dbg!(board.score());
-                dbg!(n);
-
                 winner = Some(board.score() * n);
             }
         }
