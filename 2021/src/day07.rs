ca375c775892c9d76b1212c2321f964a6f2cb855
Chris Butler <chrisbutler296@gmail.com>
2021-12-06 23:11:22 -0700

    Add closed-formula solutions for day7

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -63,3 +63,53 @@ pub fn part2(input: &str) -> i64 {
 
     cheapest
 }
+
+fn radix_sort(nums: &mut Vec<i64>) {
+    let max = 1 + *nums.iter().max().unwrap_or(&0) as usize;
+
+    let mut counts = vec![0; max];
+    for n in nums.drain(..) {
+        counts[n as usize] += 1;
+    }
+
+    for (num, count) in counts.iter().enumerate() {
+        for _ in 0..*count {
+            nums.push(num as i64);
+        }
+    }
+}
+
+#[aoc(day7, part1, formula)]
+#[inline(never)]
+pub fn part1_formula(input: &str) -> i64 {
+    let mut positions = parse_input(input);
+    radix_sort(&mut positions);
+
+    let median: i64;
+    if positions.len() % 2 == 0 {
+        let med_idx_lo = positions.len() / 2;
+        let med_idx_hi = positions.len() / 2;
+
+        median = (positions[med_idx_lo] + positions[med_idx_hi]) / 2;
+    } else {
+        median = positions[positions.len() / 2];
+    }
+
+    positions.iter().map(|p| (p - median).abs()).sum()
+}
+
+#[aoc(day7, part2, formula)]
+#[inline(never)]
+pub fn part2_formula(input: &str) -> i64 {
+    let positions = parse_input(input);
+
+    let mean = positions.iter().sum::<i64>() / positions.len() as i64;
+
+    positions
+        .iter()
+        .map(|p| {
+            let n = (p - mean).abs();
+            n * (n + 1) / 2
+        })
+        .sum()
+}
