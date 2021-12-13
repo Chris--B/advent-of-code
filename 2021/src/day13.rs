d6ee8d8897bcfdd433da23ba3211e380dc54bf4b
Chris Butler <chrisbutler296@gmail.com>
2021-12-12 23:00:04 -0700

    Unit Tests are Life

--- 2021/src/day13.rs
+++ 2021/src/day13.rs
@@ -169,3 +169,38 @@ fold along x=5"#;
 
     assert_eq!(part1(input), 17);
 }
+
+#[test]
+fn check_example_2() {
+    let input = r#"6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"#;
+
+    let zero = r#"
+#####
+#   #
+#   #
+#   #
+#####
+"#;
+
+    assert_eq!(part2(input), zero);
+}
