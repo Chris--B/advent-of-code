e2bb4ee2ef150b92e82ade96e20b0e00f9e65fb1
Chris Butler <chrisbutler296@gmail.com>
2021-12-13 23:36:58 -0700

    [Day14] Works, but no one knows why

--- 2021/src/day14.rs
+++ 2021/src/day14.rs
@@ -4,7 +4,14 @@ use itertools::Itertools;
 
 use std::collections::HashMap;
 
-fn parse_input(input: &str) -> (String, HashMap<String, String>) {
+fn str_to_pair(s: &str) -> (char, char) {
+    let mut iter = s.chars();
+    let a = iter.next().unwrap();
+    let b = iter.next().unwrap();
+    (a, b)
+}
+
+fn parse_input(input: &str) -> (String, HashMap<(char, char), char>) {
     let template = input.lines().next().unwrap().to_owned();
 
     let rules = input
@@ -13,8 +20,8 @@ fn parse_input(input: &str) -> (String, HashMap<String, String>) {
         .map(|line| {
             let mut iter = line.split(" -> ");
 
-            let from = iter.next().unwrap().to_owned();
-            let to = iter.next().unwrap().to_owned();
+            let from: (char, char) = str_to_pair(iter.next().unwrap());
+            let to: char = iter.next().unwrap().chars().next().unwrap();
             assert_eq!(iter.next(), None);
 
             (from, to)
@@ -24,16 +31,12 @@ fn parse_input(input: &str) -> (String, HashMap<String, String>) {
     (template, rules)
 }
 
-fn expand(rules: &HashMap<String, String>, s: &mut String) {
-    let mut inserts: HashMap<usize, Vec<&str>> = HashMap::new();
+fn expand(rules: &HashMap<(char, char), char>, s: &mut String) {
+    let mut inserts: HashMap<usize, Vec<char>> = HashMap::new();
 
     for ((i, a), (_, b)) in s.chars().enumerate().tuple_windows() {
-        let mut pair = String::new();
-        pair.push(a);
-        pair.push(b);
-
-        if let Some(x) = rules.get(&pair) {
-            inserts.entry(i + 1).or_insert_with(Vec::new).push(x);
+        if let Some(x) = rules.get(&(a, b)) {
+            inserts.entry(i + 1).or_insert_with(Vec::new).push(*x);
         }
     }
 
@@ -41,11 +44,9 @@ fn expand(rules: &HashMap<String, String>, s: &mut String) {
 
     // insert backwards so the index stays correct
     inserts.sort_by_key(|(idx, _)| usize::MAX - idx);
-    for (idx, texts) in inserts {
-        for text in texts {
-            for c in text.chars() {
-                s.insert(idx, c);
-            }
+    for (idx, text) in inserts {
+        for c in text {
+            s.insert(idx, c);
         }
     }
 }
@@ -53,7 +54,7 @@ fn expand(rules: &HashMap<String, String>, s: &mut String) {
 // Part1 ======================================================================
 #[aoc(day14, part1)]
 #[inline(never)]
-pub fn part1(input: &str) -> u64 {
+pub fn part1(input: &str) -> usize {
     let (mut template, rules) = parse_input(input);
 
     for _ in 0..10 {
@@ -71,24 +72,67 @@ pub fn part1(input: &str) -> u64 {
 }
 
 // Part2 ======================================================================
+fn expand_fast(template: &str, rules: &HashMap<(char, char), char>, times: usize) -> usize {
+    let mut pair_counts: HashMap<(char, char), usize> = HashMap::new();
+
+    // initialize with the template
+    for (a, b) in template.chars().tuple_windows() {
+        *pair_counts.entry((a, b)).or_insert(0) += 1;
+    }
+
+    let mut next_pair_counts = HashMap::new();
+    for _ in 0..times {
+        for ((a, b), x) in rules {
+            // "remove" our pair
+            if let Some(count) = pair_counts.get(&(*a, *b)) {
+                if *count > 0 {
+                    // "insert" its replacement pairs:
+                    let first_pair = (*a, *x);
+                    let last_pair = (*x, *b);
+
+                    *next_pair_counts.entry(first_pair).or_insert(0) += *count;
+                    *next_pair_counts.entry(last_pair).or_insert(0) += *count;
+                }
+            }
+        }
+
+        pair_counts.clear();
+        std::mem::swap(&mut next_pair_counts, &mut pair_counts);
+    }
+
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    for ((a, b), count) in pair_counts.iter() {
+        // println!("{}{}: {}", a, b, count);
+
+        *counts.entry(*a).or_insert(0) += count;
+        *counts.entry(*b).or_insert(0) += count;
+    }
+
+    // for (c, count) in counts.iter() {
+    //     println!("{}: {}", c, count);
+    // }
+
+    let most = counts.iter().map(|(_c, count)| *count).max().unwrap();
+    let least = counts.iter().map(|(_c, count)| *count).min().unwrap();
+
+    // draw the rest of the owl
+    (most - least) / 2 + 1
+}
+
+#[aoc(day14, part1, fast)]
+#[inline(never)]
+pub fn part1_fast(input: &str) -> usize {
+    let (template, rules) = parse_input(input);
+
+    expand_fast(&template, &rules, 10)
+}
+
 #[aoc(day14, part2)]
 #[inline(never)]
-pub fn part2(input: &str) -> u64 {
-    let (mut template, rules) = parse_input(input);
+pub fn part2(input: &str) -> usize {
+    let (template, rules) = parse_input(input);
 
-    for t in 0..40 {
-        dbg!(t);
-        expand(&rules, &mut template);
-    }
-
-    let mut counts = HashMap::new();
-    for c in template.chars() {
-        *counts.entry(c).or_insert(0) += 1;
-    }
-    let mut counts: Vec<_> = counts.drain().collect();
-    counts.sort_by_key(|(_c, count)| *count);
-
-    counts.last().unwrap().1 - counts.first().unwrap().1
+    expand_fast(&template, &rules, 40)
 }
 
 #[test]
@@ -128,4 +172,52 @@ CN -> C"#;
         template,
         "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
     );
+}
+
+#[test]
+fn check_example_1_fast() {
+    let input = r#"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"#;
+
+    assert_eq!(part1_fast(input), 1588);
+}
+
+#[test]
+fn check_example_2() {
+    let input = r#"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"#;
+
+    assert_eq!(part2(input), 2_188_189_693_529);
 }
