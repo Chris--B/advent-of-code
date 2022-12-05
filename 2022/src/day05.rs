a73641db417cc94a1051496799faddcb9f75c423
Chris Butler <chrisbutler296@gmail.com>
2022-12-05 15:45:57 -0700

    [2022 Day5] Speedy on 6, 88, and 200 MB inputs from the Dutch

--- 2022/src/day05.rs
+++ 2022/src/day05.rs
@@ -1,5 +1,7 @@
 use aoc_runner_derive::aoc;
 
+use smallvec::{smallvec, SmallVec};
+
 #[derive(Copy, Clone, Debug)]
 struct Move {
     count: usize,
@@ -28,18 +30,20 @@ const fn trim_ascii_start(mut bytes: &[u8]) -> &[u8] {
     bytes
 }
 
-fn fast_parse_u8(input: &[u8]) -> u8 {
+fn fast_parse_u32(input: &[u8]) -> u32 {
     debug_assert!(input.len() <= 2);
 
-    let mut bytes = [0_u8; 2];
+    let mut digits = [0_u32; 10];
+    let mut x = 1;
     for (i, b) in input.iter().rev().enumerate() {
-        bytes[i] = *b - b'0';
+        digits[i] = x * (*b - b'0') as u32;
+        x *= 10;
     }
 
-    bytes[1] * 10 + bytes[0]
+    digits.into_iter().sum()
 }
 
-fn parse(input: &str) -> State {
+fn parse_to_move(input: &str) -> State {
     let input = input.as_bytes();
 
     let crate_lines: Vec<&[u8]> = input
@@ -55,13 +59,13 @@ fn parse(input: &str) -> State {
             let mut parts = line.split(|b| *b == b' ');
 
             let _ = parts.next(); // "move"
-            let count = fast_parse_u8(parts.next().unwrap()) as usize;
+            let count = fast_parse_u32(parts.next().unwrap()) as usize;
 
             let _ = parts.next(); // "from"
-            let from = fast_parse_u8(parts.next().unwrap()) as usize;
+            let from = fast_parse_u32(parts.next().unwrap()) as usize;
 
             let _ = parts.next(); // "to"
-            let to = fast_parse_u8(parts.next().unwrap()) as usize;
+            let to = fast_parse_u32(parts.next().unwrap()) as usize;
 
             // This is a nonsense move, and could break some of our logic below.
             debug_assert_ne!(from, to);
@@ -86,10 +90,10 @@ fn parse(input: &str) -> State {
 }
 
 // Part1 ========================================================================
-#[aoc(day5, part1)]
+#[aoc(day5, part1, move_crates)]
 #[inline(never)]
 pub fn part1(input: &str) -> String {
-    let mut state = parse(input);
+    let mut state = parse_to_move(input);
 
     for Move { count, from, to } in state.moves {
         debug_assert!(state.stacks[from - 1].len() >= count);
@@ -108,11 +112,113 @@ pub fn part1(input: &str) -> String {
         .collect()
 }
 
+fn track_crates(input: &str, is_part1: bool) -> String {
+    let input = input.as_bytes();
+
+    let (crates_max_height, stack_count) = input
+        .split(|b| *b == b'\n')
+        .enumerate()
+        .find(|(_i, l)| l.starts_with(b" 1") || l.starts_with(b" 0"))
+        .map(|(i, l)| (i, l.iter().filter(|b| **b != b' ').count()))
+        .unwrap();
+
+    assert_ne!(stack_count, 0);
+
+    #[derive(Copy, Clone, Debug)]
+    struct Crate {
+        stack: usize,
+        idx: usize,
+    }
+
+    let mut crates: SmallVec<[Crate; 10]> = smallvec![];
+    for stack in 0..stack_count {
+        crates.push(Crate { stack, idx: 0 })
+    }
+
+    let mut stacks = vec![vec![]; stack_count];
+
+    for line in input.split(|b| *b == b'\n').take(crates_max_height) {
+        for (stack, letter) in line.chunks(4).enumerate() {
+            let c = letter[1];
+            if c == b' ' {
+                continue;
+            }
+
+            stacks[stack].push(c as char);
+        }
+    }
+
+    let moves = input
+        .split(|b| *b == b'\n')
+        .rev()
+        .filter(|line| !line.is_empty())
+        .take_while(|line| !line.starts_with(b" 1"))
+        .map(|line| {
+            let mut parts = line.split(|b| *b == b' ');
+
+            let _ = parts.next(); // "move"
+            let count = fast_parse_u32(parts.next().unwrap()) as usize;
+
+            let _ = parts.next(); // "from"
+            let from = fast_parse_u32(parts.next().unwrap()) as usize - 1;
+
+            let _ = parts.next(); // "to"
+            let to = fast_parse_u32(parts.next().unwrap()) as usize - 1;
+
+            debug_assert_ne!(from, to);
+
+            Move { count, from, to }
+        });
+
+    for Move { count, from, to } in moves {
+        for c in &mut crates {
+            if c.stack == to {
+                if c.idx >= count {
+                    // Crate doesn't move, adjust position
+                    c.idx -= count;
+                } else {
+                    // Crate moves
+                    c.stack = from;
+                    if is_part1 {
+                        c.idx = count - c.idx - 1;
+                    }
+                }
+            } else if c.stack == from {
+                // Crate doesn't move, adjust position
+                c.idx += count;
+            }
+        }
+    }
+
+    crates
+        .into_iter()
+        .map(|c| {
+            if let Some(c) = stacks[c.stack].get(c.idx) {
+                *c
+            } else {
+                '_'
+            }
+        })
+        .collect()
+}
+
+#[aoc(day5, part1, track_crates)]
+#[inline(never)]
+pub fn part1_track(input: &str) -> String {
+    track_crates(input, true)
+}
+
+#[aoc(day5, part2, track_crates)]
+#[inline(never)]
+pub fn part2_track(input: &str) -> String {
+    track_crates(input, false)
+}
+
 // Part2 ========================================================================
-#[aoc(day5, part2)]
+#[aoc(day5, part2, move_crates)]
 #[inline(never)]
 pub fn part2(input: &str) -> String {
-    let mut state = parse(input);
+    let mut state = parse_to_move(input);
 
     for Move { count, from, to } in state.moves {
         debug_assert!(state.stacks[from - 1].len() >= count);
@@ -146,8 +252,7 @@ mod test {
     use super::*;
     use rstest::*;
 
-    const EXAMPLE_INPUT: &str = r"
-    [D]
+    const EXAMPLE_INPUT: &str = r"    [D]
 [N] [C]
 [Z] [M] [P]
  1   2   3
@@ -163,7 +268,7 @@ move 1 from 1 to 2
     #[trace]
     fn check_ex_part_1(
         #[notrace]
-        #[values(part1)]
+        #[values(part1, part1_track)]
         p: impl FnOnce(&str) -> String,
         #[case] expected: String,
         #[case] input: &str,
@@ -176,12 +281,11 @@ move 1 from 1 to 2
     #[trace]
     fn check_ex_part_2(
         #[notrace]
-        #[values(part2)]
+        #[values(part2, part2_track)]
         p: impl FnOnce(&str) -> String,
         #[case] expected: String,
         #[case] input: &str,
     ) {
-        let input = input;
         assert_eq!(p(input), expected);
     }
 }
