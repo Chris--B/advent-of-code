e9fd9e541650bb5752a46a01686bba75eae8a2c9
Chris Butler <chrisbutler296@gmail.com>
2022-12-09 23:34:15 -0700

    [2022 Day10] Part 2!
    I like these cpu problems :)

--- 2022/src/day10.rs
+++ 2022/src/day10.rs
@@ -8,6 +8,8 @@ struct Cpu {
     reg_x: i64,
 
     signals: [i64; 6],
+
+    crt: [u8; 240],
 }
 
 impl Cpu {
@@ -16,13 +18,20 @@ impl Cpu {
             reg_cc: 0,
             reg_x: 1,
             signals: [0_i64; 6],
+            crt: [0; 240],
         }
     }
 
     fn tick(&mut self) {
+        let drawing = (self.reg_cc as i64) % 40;
+        let sprite_min = self.reg_x - 1;
+        let sprite_max = self.reg_x + 1;
+
+        if sprite_min <= drawing && drawing <= sprite_max {
+            self.crt[self.reg_cc as usize] = 1;
+        }
+
         self.reg_cc += 1;
-        // println!("[{}] X={}", self.reg_cc, self.reg_x);
-        // println!("[{}] {}", self.reg_cc, self.signal_strength());
 
         if self.reg_cc >= 20 {
             let cc = self.reg_cc - 20;
@@ -91,26 +100,43 @@ pub fn part1(input: &str) -> i64 {
 
 // Part2 ========================================================================
 #[aoc(day10, part2)]
-pub fn part2(_input: &str) -> i64 {
-    unimplemented!();
+pub fn part2(input: &str) -> SmallString<[u8; 256]> {
+    let mut cpu = Cpu::new();
+
+    for line in input.lines() {
+        match &line[..4] {
+            "addx" => cpu.addx(line[5..].parse().unwrap()),
+            "noop" => cpu.noop(),
+            _ => unreachable!("Unrecognized instruction: {}", line),
+        }
+    }
+
+    let mut output = SmallString::new();
+
+    for crt_line in cpu.crt.chunks(40) {
+        output.push('\n');
+
+        for lit in crt_line {
+            let c = if *lit != 0 { '#' } else { '.' };
+            output.push(c);
+        }
+    }
+    debug_assert!(output.len() < 256);
+
+    output
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    #[allow(unused_imports)]
+    use pretty_assertions::{assert_eq, assert_ne};
     use rstest::*;
-
-    const SHORT_EXAMPLE: &str = r"
-noop
-addx 3
-addx -5
-";
 
     const EXAMPLE_INPUT: &str = include_str!("../input/2022/day10_example.txt");
 
     #[rstest]
     #[case::given(13_140, EXAMPLE_INPUT)]
-    // #[case::given(13_140, SHORT_EXAMPLE)]
     #[trace]
     fn check_ex_part_1(
         #[notrace]
@@ -123,17 +149,27 @@ addx -5
         assert_eq!(p(input), expected);
     }
 
-    // #[rstest]
-    // #[case::given(999_999, EXAMPLE_INPUT)]
-    // #[trace]
-    // fn check_ex_part_2(
-    //     #[notrace]
-    //     #[values(part2)]
-    //     p: impl FnOnce(&str) -> i64,
-    //     #[case] expected: i64,
-    //     #[case] input: &str,
-    // ) {
-    //     let input = input.trim();
-    //     assert_eq!(p(input), expected);
-    // }
+    const EXAMPLE_OUTPUT: &str = r"
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....";
+
+    #[rstest]
+    #[case::given(EXAMPLE_OUTPUT, EXAMPLE_INPUT)]
+    #[trace]
+    fn check_ex_part_2(
+        #[notrace]
+        #[values(part2)]
+        p: impl FnOnce(&str) -> SmallString<[u8; 256]>,
+        #[case] expected: &str,
+        #[case] input: &str,
+    ) {
+        let input = input.trim();
+        let output = p(input);
+        println!("output:\n{output}",);
+        assert_eq!(output, expected);
+    }
 }
