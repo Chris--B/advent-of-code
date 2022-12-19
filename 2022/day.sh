3b4fec70bf66a68946493148957d10ea5551b83f
Chris Butler <chrisbutler296@gmail.com>
2022-12-18 20:56:11 -0700

    Update day.sh to use the right timezone

--- 2022/day.sh
+++ 2022/day.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-d=$(date +%d)
-dd=$(date +%0d)
+d=$(TZ="America/Boston" date +%d)
+dd=$(TZ="America/Boston" date +%0d)
 
 cp -v src/day00.rs src/day$dd.rs
 sed "s/dayN/day$d/" -i src/day$dd.rs
