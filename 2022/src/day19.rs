aa5aa4cbff1198085f617cce85a966d09e934d4c
Chris Butler <chrisbutler296@gmail.com>
2022-12-19 19:04:11 -0700

    [2022 Day19] Promising!

--- 2022/src/day19.rs
+++ 2022/src/day19.rs
@@ -2,28 +2,28 @@
 
 use crate::prelude::*;
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Blueprint {
     // Each recipe in order: ORE, CLAY, OBSIDIAN, GEODE
-    ore: [i32; 4],
-    clay: [i32; 4],
-    obsidian: [i32; 4],
-    geode: [i32; 4],
+    ore: [u8; 4],
+    clay: [u8; 4],
+    obsidian: [u8; 4],
+    geode: [u8; 4],
 }
 
 // const R_NAMES: [&str; 4] = ["ore", "clay", "obsidian", "geode"];
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 enum Resource {
-    Ore,
-    Clay,
-    Obsidian,
-    Geode,
+    Ore = 0,
+    Clay = 1,
+    Obsidian = 2,
+    Geode = 3,
 }
 use Resource::*;
 
 impl Resource {
-    fn index(&self) -> usize {
+    const fn index(&self) -> usize {
         match *self {
             Ore => 0,
             Clay => 1,
@@ -38,13 +38,13 @@ fn parse_blueprint(line: &str) -> Blueprint {
     let recipes: [&str; 4] = iter_to_array(line.split('.').map(|s| s.trim()));
 
     let ore = {
-        let x = scan_fmt!(recipes[0], "Each ore robot costs {} ore", i32).unwrap();
+        let x = scan_fmt!(recipes[0], "Each ore robot costs {} ore", u8).unwrap();
         // ORE, CLAY, OBSIDIAN, GEODE
         [x, 0, 0, 0]
     };
 
     let clay = {
-        let x = scan_fmt!(recipes[1], "Each clay robot costs {} ore", i32).unwrap();
+        let x = scan_fmt!(recipes[1], "Each clay robot costs {} ore", u8).unwrap();
         [x, 0, 0, 0]
     };
 
@@ -52,8 +52,8 @@ fn parse_blueprint(line: &str) -> Blueprint {
         let (x, y) = scan_fmt!(
             recipes[2],
             "Each obsidian robot costs {} ore and {} clay",
-            i32,
-            i32
+            u8,
+            u8
         )
         .unwrap();
         [x, y, 0, 0]
@@ -63,8 +63,8 @@ fn parse_blueprint(line: &str) -> Blueprint {
         let (x, z) = scan_fmt!(
             recipes[3],
             "Each geode robot costs {} ore and {} obsidian",
-            i32,
-            i32
+            u8,
+            u8
         )
         .unwrap();
         [x, 0, z, 0]
@@ -83,141 +83,180 @@ fn parse(input: &str) -> Vec<Blueprint> {
     input.lines().map(parse_blueprint).collect()
 }
 
-fn try_spend(bank: &mut [i32; 4], cost: &[i32; 4]) -> bool {
-    for i in 0..4 {
-        if bank[i] < cost[i] {
-            return false;
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct SimState {
+    minute: u8,
+    bank: [u8; 4],
+    bots: [u8; 4],
+    bp: Blueprint,
+}
+
+impl SimState {
+    fn new(bp: &Blueprint) -> Self {
+        SimState {
+            minute: 1,
+            bank: [0; 4],
+            bots: [1, 0, 0, 0],
+            bp: *bp,
         }
     }
 
-    for i in 0..4 {
-        bank[i] -= cost[i]
+    fn can_afford(&self, resource: Resource) -> bool {
+        let cost = match resource {
+            Ore => &self.bp.ore,
+            Clay => &self.bp.clay,
+            Obsidian => &self.bp.obsidian,
+            Geode => &self.bp.geode,
+        };
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..4 {
+            if self.bank[i] < cost[i] {
+                return false;
+            }
+        }
+
+        true
     }
 
-    true
-}
+    fn step_until(&mut self, resource: Resource) -> bool {
+        // Sanity check if we can EVER afford this
+        let cost = match resource {
+            Ore => &self.bp.ore,
+            Clay => &self.bp.clay,
+            Obsidian => &self.bp.obsidian,
+            Geode => &self.bp.geode,
+        };
 
-fn compute_quality_level(bp: &Blueprint, mut build_order: &[Resource]) -> usize {
-    let mut bank = [0, 0, 0, 0];
-    let mut bots = [1, 0, 0, 0];
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..4 {
+            if (self.bots[i] == 0) && (cost[i] != 0) {
+                return false;
+            }
+        }
 
-    for _m in 1..=24 {
-        // println!("== Minute {_m} ==");
+        while self.minute <= 24 {
+            if self.step(resource) {
+                return true;
+            }
+        }
 
-        // Start constructing a new bot if we can build it yet
+        false
+    }
+
+    /// Returns true if a bot was built. If it cannot afford one, it still steps but waits instead.
+    fn step(&mut self, resource: Resource) -> bool {
+        assert!(self.minute <= 24);
+
+        let mut built_bot = false;
         let mut pending_robot = None;
-        if let Some(next) = build_order.first() {
-            let cost = match next {
-                Ore => &bp.ore,
-                Clay => &bp.clay,
-                Obsidian => &bp.obsidian,
-                Geode => &bp.geode,
+
+        if self.can_afford(resource) {
+            // Start constructing a new bot if we can build it yet
+            let cost = match resource {
+                Ore => &self.bp.ore,
+                Clay => &self.bp.clay,
+                Obsidian => &self.bp.obsidian,
+                Geode => &self.bp.geode,
             };
 
-            // println!("** Trying to build {:?}", next);
-            if try_spend(&mut bank, cost) {
-                pending_robot = Some(next.index());
-                build_order = &build_order[1..];
+            // Pay for the bot
+            #[allow(clippy::needless_range_loop)]
+            for i in 0..4 {
+                self.bank[i] -= cost[i];
             }
+
+            // And finally, the construction
+            pending_robot = Some(resource.index());
+            built_bot = true;
         }
 
         // Mine the resources
-
         for i in 0..4 {
-            if bots[i] > 0 {
-                bank[i] += bots[i];
-
-                // let have = bank[i];
-                // let bot_count = bots[i];
-                // let resource = R_NAMES[i];
-                // println!("{bot_count} {resource} robot collects {bot_count} {resource}; you now have {have} {resource}.");
-            }
+            self.bank[i] += self.bots[i];
         }
 
         // Our new construction finished!
-
         if let Some(idx) = pending_robot {
-            bots[idx] += 1;
-
-            // let robot = R_NAMES[idx];
-            // let count = bots[idx];
-            // println!("The new {robot}-collecting robot is ready; you now have {count} of them.");
+            self.bots[idx] += 1;
         }
 
-        // println!();
+        self.minute += 1;
+
+        built_bot
+    }
+}
+
+fn find_best_build(bp: &Blueprint) -> usize {
+    // Never build more bots than we can spend in a single minute
+    let max_ore_bots = [
+        bp.clay[Ore.index()],
+        bp.obsidian[Ore.index()],
+        bp.geode[Ore.index()],
+    ]
+    .into_iter()
+    .max()
+    .unwrap();
+    let max_clay_bots = bp.obsidian[Clay.index()];
+    let max_obsidian_bots = bp.geode[Obsidian.index()];
+
+    let mut best_ql = 0;
+
+    let mut queue: VecDeque<SimState> = VecDeque::new();
+    queue.push_back(SimState::new(bp));
+
+    while let Some(sim) = queue.pop_front() {
+        if sim.minute > 24 {
+            best_ql = best_ql.max(sim.bank[Geode.index()]);
+            continue;
+        }
+
+        // Continue searching...
+
+        if sim.bots[Ore.index()] < max_ore_bots {
+            let mut sim = sim;
+            if sim.step_until(Ore) {
+                // We were able to buy it, so continue this path
+                queue.push_back(sim);
+            }
+        }
+
+        if sim.bots[Clay.index()] < max_clay_bots {
+            let mut sim = sim;
+            if sim.step_until(Clay) {
+                // We were able to buy it, so continue this path
+                queue.push_back(sim);
+            }
+        }
+
+        if sim.bots[Obsidian.index()] < max_obsidian_bots {
+            let mut sim = sim;
+            if sim.step_until(Obsidian) {
+                // We were able to buy it, so continue this path
+                queue.push_back(sim);
+            }
+        }
+
+        // Always try to make a Geode! 🤑
+        {
+            let mut sim = sim;
+            if sim.step_until(Geode) {
+                // We were able to buy it, so continue this path
+                queue.push_back(sim);
+            }
+        }
     }
 
-    bank[3] as usize
+    best_ql as usize
 }
-
-type Build = SmallVec<[Resource; 24]>;
-
-fn append_and_clone(build: &Build, r: Resource) -> Build {
-    let mut build = build.clone();
-    build.push(r);
-    build
-}
-
 // Part1 ========================================================================
 #[aoc(day19, part1)]
 pub fn part1(blueprints: &[Blueprint]) -> usize {
     blueprints
         .iter()
-        .map(|bp| {
-            println!("{bp:?}");
-
-            let mut best = 0;
-
-            let mut builds = VecDeque::new();
-            builds.push_back(smallvec![Ore]);
-            builds.push_back(smallvec![Clay]);
-
-            let mut i = 0;
-            while let Some(build) = builds.pop_front() {
-                i += 1;
-
-                if build.len() >= 8 {
-                    let score = compute_quality_level(bp, &build);
-                    if score >= best {
-                        println!("[{i}] {best} -> {score}: {build:?}");
-                    }
-                    best = best.max(score);
-                }
-
-                // Build new builds
-                if build.len() < 10 {
-                    let income = build
-                        .iter()
-                        .map(|r| match r {
-                            Ore => bp.ore,
-                            Clay => bp.clay,
-                            Obsidian => bp.obsidian,
-                            Geode => bp.geode,
-                        })
-                        .fold([0; 4], |mut acc, x| {
-                            for i in 0..4 {
-                                acc[i] += x[i];
-                            }
-                            acc
-                        });
-
-                    if !try_spend(&mut income.clone(), &bp.ore) {
-                        builds.push_back(append_and_clone(&build, Ore));
-                    }
-
-                    builds.push_back(append_and_clone(&build, Clay));
-
-                    builds.push_back(append_and_clone(&build, Obsidian));
-
-                    builds.push_back(append_and_clone(&build, Geode));
-                }
-            }
-            println!();
-
-            best
-        })
+        .map(find_best_build)
         .enumerate()
-        .map(|(id, ql)| (id + 1) * ql)
+        .map(|(i, ql)| (i + 1) * ql)
         .sum()
 }
 
@@ -247,10 +286,15 @@ Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsid
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
 ";
 
+    const PROBLEM_INPUT: &str = r"
+Blueprint 28: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 7 clay. Each geode robot costs 2 ore and 7 obsidian.
+";
+
     #[rstest]
     #[case::given(33, EXAMPLE_INPUT)]
     #[case::blueprint_1(9, EXAMPLE_INPUT_BP1)]
     #[case::blueprint_2(12, EXAMPLE_INPUT_BP2)]
+    #[case::problem(13, PROBLEM_INPUT)]
     #[trace]
     fn check_ex_part_1(
         #[notrace]
--- 2022/src/lib.rs
+++ 2022/src/lib.rs
@@ -22,7 +22,7 @@ pub mod day15;
 pub mod day16;
 pub mod day17;
 pub mod day18;
-#[cfg(feature = "broken")]
+// #[cfg(feature = "broken")]
 pub mod day19;
 
 pub mod framebuffer;
