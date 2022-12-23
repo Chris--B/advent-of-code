6f233a156e13817ab309c64d84832aab34298bdd
Chris Butler <chrisbutler296@gmail.com>
2022-12-23 11:33:08 -0700

    [2022 Day23] 🎉 Part 2

--- 2022/src/day23.rs
+++ 2022/src/day23.rs
@@ -178,10 +178,119 @@ pub fn part1(input: &str) -> i32 {
 }
 
 // Part2 ========================================================================
-// #[aoc(day23, part2)]
-// pub fn part2(input: &str) -> i32 {
-//     let grid = parse(input);
-// }
+#[aoc(day23, part2)]
+pub fn part2(input: &str) -> i32 {
+    let mut grid: HashSet<IVec2> = parse(input);
+
+    let mut queue: [[IVec2; 3]; 4] = [
+        [DIR_N, DIR_NE, DIR_NW],
+        [DIR_S, DIR_SE, DIR_SW],
+        [DIR_W, DIR_NW, DIR_SW],
+        [DIR_E, DIR_NE, DIR_SE],
+    ];
+
+    let (min_x, max_x) = (-3, 10);
+    let (min_y, max_y) = (-3, 8);
+
+    for round in 0.. {
+        if cfg!(test) {
+            println!();
+            println!("=== Round {round} ====");
+
+            println!();
+            println!("x: {min_x}..{max_x} ({})", max_x - min_x);
+            println!("y: {min_y}..{max_y} ({})", max_y - min_y);
+            println!("{} elves", grid.len());
+            for y in (min_y..=max_y).rev() {
+                for x in min_x..=max_x {
+                    if grid.contains(&(x, y).into()) {
+                        print!("#");
+                    } else {
+                        print!(".");
+                    }
+                }
+                println!();
+            }
+            println!();
+        }
+
+        let mut n_moves = 0;
+        let mut moves = HashMap::<IVec2, u32>::new();
+
+        // Each elf proposes a move to make
+        'elves: for elf in grid.iter().copied() {
+            // If the elf has NO neighbors, do nothing
+            if elf.full_neighbors().iter().all(|x| !grid.contains(x)) {
+                continue 'elves;
+            }
+
+            for [a, b, c] in &queue {
+                if !grid.contains(&(elf + *a))
+                    && !grid.contains(&(elf + *b))
+                    && !grid.contains(&(elf + *c))
+                {
+                    *moves.entry(elf + *a).or_insert(0) += 1;
+                    continue 'elves;
+                }
+            }
+
+            // If all of that fails, our elf has no moves and stays put
+        }
+
+        let mut next_grid = HashSet::new();
+
+        // Each elf makes that move if no one else tries to move there
+        'elves: for elf in grid.iter().copied() {
+            // If the elf has NO neighbors, do nothing
+            if elf.full_neighbors().iter().all(|x| !grid.contains(x)) {
+                next_grid.insert(elf);
+                continue 'elves;
+            }
+
+            for [a, b, c] in &queue {
+                if !grid.contains(&(elf + *a))
+                    && !grid.contains(&(elf + *b))
+                    && !grid.contains(&(elf + *c))
+                {
+                    // This is a valid move, so we will either move or not.
+                    // We make that choice here.
+                    if moves[&(elf + *a)] == 1 {
+                        n_moves += 1;
+                        next_grid.insert(elf + *a);
+                    } else {
+                        next_grid.insert(elf);
+                    }
+
+                    // And stop considering other moves
+                    continue 'elves;
+                }
+            }
+
+            // If all of that fails, our elf has no moves and stays put
+            next_grid.insert(elf);
+        }
+
+        // "Rotate" the move-direction queue
+        {
+            let first = queue[0];
+            queue.rotate_left(1);
+            queue[3] = first;
+        }
+
+        debug_assert_eq!(
+            grid.len(),
+            next_grid.len(),
+            "Conservation of Elf has failed!"
+        );
+        grid = next_grid;
+
+        if n_moves == 0 {
+            return round + 1;
+        }
+    }
+
+    unreachable!()
+}
 
 #[cfg(test)]
 mod test {
@@ -224,17 +333,18 @@ mod test {
         assert_eq!(p(input), expected);
     }
 
-    // #[rstest]
-    // #[case::given(999_999, EXAMPLE_INPUT)]
-    // #[trace]
-    // fn check_ex_part_2(
-    //     #[notrace]
-    //     #[values(part2)]
-    //     p: impl FnOnce(&str) -> i32,
-    //     #[case] expected: i32,
-    //     #[case] input: &str,
-    // ) {
-    //     let input = input.trim();
-    //     assert_eq!(p(input), expected);
-    // }
+    #[rstest]
+    #[case::given(20, EXAMPLE_INPUT)]
+    #[case::given_tiny(4, EXAMPLE_INPUT_TINY)]
+    #[trace]
+    fn check_ex_part_2(
+        #[notrace]
+        #[values(part2)]
+        p: impl FnOnce(&str) -> i32,
+        #[case] expected: i32,
+        #[case] input: &str,
+    ) {
+        let input = input.trim();
+        assert_eq!(p(input), expected);
+    }
 }
