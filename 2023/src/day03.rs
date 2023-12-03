fa5598d37568ee073967ec3db355cc97a92e3e21
Chris Butler <chrisbutler296@gmail.com>
2023-12-02 22:55:11 -0700

    It got worse

--- 2023/src/day03.rs
+++ 2023/src/day03.rs
@@ -109,8 +109,85 @@ pub fn part1(input: &str) -> i64 {
 
 // Part2 ========================================================================
 #[aoc(day3, part2)]
-pub fn part2(_input: &str) -> i64 {
-    unimplemented!();
+pub fn part2(input: &str) -> i64 {
+    let grid = parse(input);
+
+    let (min_x, max_x) = grid
+        .keys()
+        .copied()
+        .map(|(x, _y)| x)
+        .minmax()
+        .into_option()
+        .unwrap();
+    let (min_y, max_y) = grid
+        .keys()
+        .copied()
+        .map(|(_x, y)| y)
+        .minmax()
+        .into_option()
+        .unwrap();
+
+    let mut nums = vec![];
+
+    for y in min_y..=max_y {
+        let mut x = min_x;
+        while x <= max_x {
+            if let Some(c) = grid.get(&(x, y)) {
+                if c.is_ascii_digit() {
+                    let mut xx = x;
+                    while let Some(c) = grid.get(&(xx, y)) {
+                        if !c.is_ascii_digit() {
+                            break;
+                        }
+                        xx += 1;
+                    }
+
+                    let num: i32 = extract_num(input, x, xx, y, if cfg!(test) { 11 } else { 141 });
+                    nums.push((x, y, num, xx - x));
+
+                    x = xx;
+                    continue;
+                }
+            }
+            x += 1;
+        }
+    }
+    assert!(!nums.is_empty());
+
+    let mut next_to_counts = HashMap::new();
+    let mut next_to_nums = HashMap::new();
+
+    'nums_loop: for (x, y, num, n) in nums.iter().copied() {
+        let mut to_check = HashSet::new();
+        for x in x..(x + n) {
+            for dy in [-1, 0, 1] {
+                for dx in [-1, 0, 1] {
+                    to_check.insert((x + dx, y + dy));
+                }
+            }
+        }
+
+        for (xx, yy) in to_check {
+            if let Some(c) = grid.get(&(xx, yy)) {
+                if !c.is_ascii_digit() {
+                    *next_to_counts.entry((xx, yy)).or_insert(0) += 1;
+                    next_to_nums.entry((xx, yy)).or_insert(vec![]).push(num);
+                    continue 'nums_loop;
+                }
+            }
+        }
+    }
+
+    next_to_counts
+        .iter()
+        .filter_map(|(xy, count)| -> Option<i64> {
+            if *count == 2 {
+                Some(next_to_nums[xy].iter().copied().product::<i32>() as i64)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -147,17 +224,17 @@ mod test {
         assert_eq!(p(input), expected);
     }
 
-    // #[rstest]
-    // #[case::given(999_999, EXAMPLE_INPUT)]
-    // #[trace]
-    // fn check_ex_part_2(
-    //     #[notrace]
-    //     #[values(part2)]
-    //     p: impl FnOnce(&str) -> i64,
-    //     #[case] expected: i64,
-    //     #[case] input: &str,
-    // ) {
-    //     let input = input.trim();
-    //     assert_eq!(p(input), expected);
-    // }
+    #[rstest]
+    #[case::given(467835, EXAMPLE_INPUT)]
+    #[trace]
+    fn check_ex_part_2(
+        #[notrace]
+        #[values(part2)]
+        p: impl FnOnce(&str) -> i64,
+        #[case] expected: i64,
+        #[case] input: &str,
+    ) {
+        let input = input.trim();
+        assert_eq!(p(input), expected);
+    }
 }
