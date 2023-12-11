9d96442e72ccf5d93ecb012285e9879e598ed264
Chris Butler <chrisbutler296@gmail.com>
2023-12-11 11:21:57 -0700

    Do expansion in-place instead of making a whole new vec

--- 2023/src/day11.rs
+++ 2023/src/day11.rs
@@ -16,39 +16,37 @@ fn parse(input: &str) -> Vec<(i64, i64)> {
     galaxies
 }
 
-fn do_with_expansion(galaxies: &[(i64, i64)], expansion: i64) -> i64 {
+fn do_with_expansion(galaxies: &mut [(i64, i64)], expansion: i64) -> i64 {
     let mut is_expanded_x = [true; 200];
     let mut is_expanded_y = [true; 200];
 
-    for (x, y) in galaxies {
+    for (x, y) in &*galaxies {
         is_expanded_x[*x as usize] = false;
         is_expanded_y[*y as usize] = false;
     }
 
-    let galaxies: Vec<_> = galaxies
-        .iter()
-        .map(|p| {
-            let mut warped_x = 0;
-            for x in 0..p.0 {
-                if is_expanded_x[x as usize] {
-                    warped_x += expansion;
-                } else {
-                    warped_x += 1;
-                }
+    for p in &mut *galaxies {
+        let mut warped_x = 0;
+        for x in 0..p.0 {
+            if is_expanded_x[x as usize] {
+                warped_x += expansion;
+            } else {
+                warped_x += 1;
             }
+        }
 
-            let mut warped_y = 0;
-            for y in 0..p.1 {
-                if is_expanded_y[y as usize] {
-                    warped_y += expansion;
-                } else {
-                    warped_y += 1;
-                }
+        let mut warped_y = 0;
+        for y in 0..p.1 {
+            if is_expanded_y[y as usize] {
+                warped_y += expansion;
+            } else {
+                warped_y += 1;
             }
+        }
 
-            (warped_x, warped_y)
-        })
-        .collect();
+        // Modify in-place, after we do all of our looping
+        *p = (warped_x, warped_y)
+    }
 
     let mut total = 0;
     for (a, b) in galaxies.iter().cartesian_product(galaxies.iter()) {
@@ -66,15 +64,15 @@ fn do_with_expansion(galaxies: &[(i64, i64)], expansion: i64) -> i64 {
 // Part1 ========================================================================
 #[aoc(day11, part1)]
 pub fn part1(input: &str) -> i64 {
-    let galaxies = parse(input);
-    do_with_expansion(&galaxies, 2)
+    let mut galaxies = parse(input);
+    do_with_expansion(&mut galaxies, 2)
 }
 
 // Part2 ========================================================================
 #[aoc(day11, part2)]
 pub fn part2(input: &str) -> i64 {
-    let galaxies = parse(input);
-    do_with_expansion(&galaxies, 1_000_000)
+    let mut galaxies = parse(input);
+    do_with_expansion(&mut galaxies, 1_000_000)
 }
 
 #[cfg(test)]
@@ -117,8 +115,8 @@ mod test {
     #[trace]
     fn check_ex_part_2(#[case] expansion: i64, #[case] expected: i64, #[case] input: &str) {
         let input = input.trim();
-        let galaxies = parse(input);
+        let mut galaxies = parse(input);
 
-        assert_eq!(do_with_expansion(&galaxies, expansion), expected);
+        assert_eq!(do_with_expansion(&mut galaxies, expansion), expected);
     }
 }
