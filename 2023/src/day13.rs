e7e7f223a1282aa779cdb41719e1e8aa43b14ed4
Chris Butler <chrisbutler296@gmail.com>
2023-12-13 11:14:20 -0700

    Day13 Part 2

--- 2023/src/day13.rs
+++ 2023/src/day13.rs
@@ -14,18 +14,17 @@ fn find_mirrored_axis(rows: &[String], cols: &[String]) -> i64 {
             let ra = &ra[(ra.len() - n)..];
             let rb = &rb[..n];
 
-            assert_eq!(ra.len(), n);
-            assert_eq!(rb.len(), n);
-
+            let mut diffs = 0;
             for (ca, cb) in ra.iter().zip(rb.iter().rev()) {
-                assert!(!ca.is_empty());
-                assert!(!cb.is_empty());
                 if ca != cb {
+                    diffs += 1;
                     continue 'split;
                 }
             }
 
-            return scale * split as i64;
+            if diffs == 0 {
+                return scale * split as i64;
+            }
         }
     }
 
@@ -77,18 +76,86 @@ pub fn part1(input: &str) -> i64 {
         sum += find_mirrored_axis(&rows, &cols);
     }
 
-    if !cfg!(test) {
-        assert!(sum > 31997);
-        assert!(![32239].contains(&sum), "{sum} was wrong before");
+    sum
+}
+
+fn find_smudge_mirrored_axis(rows: &[String], cols: &[String]) -> i64 {
+    for (scale, strings) in [(100, rows), (1, cols)] {
+        'split: for split in 1..strings.len() {
+            let (ra, rb) = strings.split_at(split);
+            let n = ra.len().min(rb.len());
+            if n == 0 {
+                continue;
+            }
+
+            let ra = &ra[(ra.len() - n)..];
+            let rb = &rb[..n];
+
+            let mut diffs = 0;
+            for (ca, cb) in ra.iter().zip(rb.iter().rev()) {
+                for (a, b) in ca.chars().zip(cb.chars()) {
+                    if a != b {
+                        diffs += 1;
+                    }
+                }
+            }
+
+            info!("[s={split}, n={n}] diffs={diffs} w/ {ra:#?}, {rb:#?}");
+            if diffs == 1 {
+                return scale * split as i64;
+            }
+        }
     }
 
-    sum
+    unreachable!()
 }
 
 // Part2 ========================================================================
 #[aoc(day13, part2)]
 pub fn part2(input: &str) -> i64 {
-    0
+    let mut sum = 0;
+
+    let blocks = input.lines().group_by(|l| l.trim().is_empty());
+    for (is_empty, block) in &blocks {
+        if is_empty {
+            continue;
+        }
+
+        let mut rows: Vec<String> = vec![];
+        let mut cols: Vec<String> = vec![];
+        cols.resize_with(20, String::new);
+
+        if log_enabled!(Info) {
+            println!();
+            println!("              10        20");
+            println!("     123456789_123456789_123456789_");
+        }
+
+        for (y, line) in block.into_iter().enumerate() {
+            if log_enabled!(Info) {
+                println!("[{:>2}] {line}", y + 1);
+            }
+
+            if cols.len() > line.len() {
+                cols.resize_with(line.len(), String::new);
+            }
+
+            // Save the row directly
+            rows.push(line.to_string());
+
+            // Save (part of) each column
+            for (x, c) in line.chars().enumerate() {
+                cols[x].push(c)
+            }
+        }
+        if log_enabled!(Info) {
+            println!();
+        }
+
+        sum += find_smudge_mirrored_axis(&rows, &cols);
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -150,11 +217,11 @@ mod test {
 ";
 
     #[rstest]
-    // #[case::given_405(405, EXAMPLE_INPUT)]
+    #[case::given_405(405, EXAMPLE_INPUT)]
     #[case::half_a(5, EXAMPLE_INPUT_HALF_A)]
-    // #[case::half_b(400, EXAMPLE_INPUT_HALF_B)]
+    #[case::half_b(400, EXAMPLE_INPUT_HALF_B)]
     #[case::mine_x(1, CASE_X_1_2)]
-    // #[case::mine_y(100, CASE_Y_1_2)]
+    #[case::mine_y(100, CASE_Y_1_2)]
     #[trace]
     fn check_ex_part_1(
         #[notrace]
@@ -167,9 +234,10 @@ mod test {
         assert_eq!(p(input), expected);
     }
 
-    #[ignore]
     #[rstest]
-    #[case::given(999_999, EXAMPLE_INPUT)]
+    #[case::given(400, EXAMPLE_INPUT)]
+    #[case::half_a(300, EXAMPLE_INPUT_HALF_A)]
+    #[case::half_b(100, EXAMPLE_INPUT_HALF_B)]
     #[trace]
     fn check_ex_part_2(
         #[notrace]
