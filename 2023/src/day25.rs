24677e988e2b8760042dfe095931ac59b937a32d
Chris Butler <chrisbutler296@gmail.com>
2023-12-27 16:15:14 -0700

    Save a day25 viz - still need to research graph cutting

--- 2023/src/day25.rs
+++ 2023/src/day25.rs
@@ -20,6 +20,10 @@ pub fn part1(input: &str) -> i64 {
         }
     }
 
+    if cfg!(test) {
+        save_adj_matrix(&graph);
+    }
+
     // haaaaaaaaaaahahahaha
     let cuts = if cfg!(test) {
         [("hfx", "pzl"), ("bvb", "cmg"), ("nvd", "jqt")]
@@ -74,6 +78,51 @@ pub fn part1(input: &str) -> i64 {
     }
 
     (seen.len() * (graph.len() - seen.len())) as i64
+}
+
+fn save_adj_matrix(graph: &HashMap<&str, Vec<&str>>) {
+    use image::imageops::{self, FilterType};
+    use image::{Rgb, RgbImage};
+
+    let mut keys: Vec<&str> = graph.keys().copied().collect_vec();
+    keys.sort_by_key(|k| (graph[k].len(), *k));
+
+    let dim = keys.len() as u32;
+    let mut frame = RgbImage::from_fn(dim, dim, |x, y| {
+        let from_node = keys[x as usize];
+        let to_node = keys[y as usize];
+        if graph[from_node].contains(&to_node) {
+            Rgb([0xff, 0xff, 0xff])
+        } else {
+            Rgb([0; 3])
+        }
+    });
+
+    let cuts = if cfg!(test) {
+        [("hfx", "pzl"), ("bvb", "cmg"), ("nvd", "jqt")]
+    } else {
+        [("kkp", "vtv"), ("jll", "lnf"), ("cmj", "qhd")]
+    };
+
+    for (from_node, to_node) in cuts {
+        let x = keys.iter().position(|n| *n == from_node).unwrap() as u32;
+        let y = keys.iter().position(|n| *n == to_node).unwrap() as u32;
+
+        *frame.get_pixel_mut(x, y) = Rgb([0xff, 0x00, 0xff]);
+        *frame.get_pixel_mut(y, x) = Rgb([0xff, 0x00, 0xff]);
+    }
+
+    let new_dim = (2048 / dim) * dim;
+    warn!("Scaling adj matrix from {dim}x{dim} -> {new_dim}x{new_dim}");
+    assert!(new_dim > 0);
+
+    let frame = imageops::resize(&frame, new_dim, new_dim, FilterType::Nearest);
+    let filename = if cfg!(test) {
+        "/Users/chris/code/me/advent-of-code/2023/target/day25-adj-matrix_test.png"
+    } else {
+        "/Users/chris/code/me/advent-of-code/2023/target/day25-adj-matrix.png"
+    };
+    frame.save(filename).unwrap();
 }
 
 #[cfg(test)]
