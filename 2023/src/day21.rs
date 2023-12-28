470b1ef71c48dfa947f9e5ba9afa41c984907951
Chris Butler <chrisbutler296@gmail.com>
2023-12-28 14:04:32 -0700

    Failed attempt at power regression

--- 2023/src/day21.rs
+++ 2023/src/day21.rs
@@ -2,6 +2,8 @@
 
 use crate::prelude::*;
 
+use std::ops::Add;
+
 type Grid = Framebuffer<char>;
 
 fn parse(input: &str) -> (IVec2, Grid) {
@@ -58,81 +60,183 @@ pub fn part1(input: &str) -> i64 {
 }
 
 // Part2 ========================================================================
-fn do_part2(steps: i64, input: &str) -> i64 {
-    let (start, grid) = parse(input);
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct MetaPoint {
+    in_tile: IVec2,
+    tile: IVec2,
+}
 
+impl MetaPoint {
+    fn refit(mut self, nx: i32, ny: i32) -> Self {
+        // Adjust x if it's outside of its tile
+        let x = self.in_tile.x;
+        if !(0..nx).contains(&x) {
+            self.in_tile.x = wrap(x, nx);
+
+            // and adjust the tile
+            if x >= nx {
+                self.tile.x += 1;
+            } else if x < 0 {
+                self.tile.x -= 1;
+            } else {
+                unreachable!();
+            }
+        };
+
+        // Adjust y if it's outside of its tile
+        let y = self.in_tile.y;
+        if !(0..ny).contains(&y) {
+            self.in_tile.y = wrap(y, ny);
+
+            // and adjust the tile
+            if y >= ny {
+                self.tile.y += 1;
+            } else if y < 0 {
+                self.tile.y -= 1;
+            } else {
+                unreachable!();
+            }
+        };
+
+        self
+    }
+}
+
+impl Add<Cardinal> for MetaPoint {
+    type Output = Self;
+    fn add(mut self, rhs: Cardinal) -> Self {
+        self.in_tile += rhs.into();
+        self
+    }
+}
+
+fn wrap(a: i32, n: i32) -> i32 {
+    // Keep the indices positive
+    ((a % n) + n) % n
+}
+
+fn do_part2_sim(steps: i64, input: &str) -> i64 {
+    let (start, grid) = parse(input);
     let nx = grid.width() as i32;
     let ny = grid.height() as i32;
 
-    let mut ps: Vec<IVec2> = vec![start];
-    let mut seen_this_step: Vec<IVec2> = vec![];
+    let start = MetaPoint {
+        in_tile: start,
+        tile: IVec2::zero(),
+    };
+
+    let mut ps: HashSet<MetaPoint> = [start].into();
+    let mut seen_this_step: HashSet<MetaPoint> = HashSet::new();
+
+    let spacing = false;
 
     for step in 1..=steps {
-        for p in ps.drain(..) {
+        for p in ps.drain() {
             for dir in Cardinal::ALL_NO_DIAG {
-                let next = p + dir.into();
+                let next = (p + dir).refit(nx, ny);
 
-                // In part 2, the map is infinitely repeating so handle that when indexing
-                // TODO: Ask Framebuffer to do this for us
-                fn w(a: i32, n: i32) -> i32 {
-                    // Keep the indices positive
-                    ((a % n) + n) % n
-                }
-                if grid[(w(next.x, nx), w(next.y, ny))] == '#' {
+                if grid[next.in_tile] == '#' {
                     continue;
                 }
 
                 if !seen_this_step.contains(&next) {
-                    seen_this_step.push(next);
+                    seen_this_step.insert(next);
                 }
             }
         }
 
-        // info!("[step {step:2>}] plots={}", seen_this_step.len());
-        ps.append(&mut seen_this_step);
+        for p in seen_this_step.drain() {
+            ps.insert(p);
+        }
 
-        const STEPS: &[i64] = &[1, 10, 15];
-        if cfg!(test) && STEPS.contains(&step) {
-            // Render a 3x3 grid of the maps
-            // mx and my will be the map coordinates
-            println!("step={step} ===================");
-            for my in -1..2 {
-                for y in grid.range_y() {
-                    // "full" coordinate
-                    let yy = my * ny + y;
-                    for mx in -1..2 {
-                        for x in grid.range_x() {
-                            // "full" coordinate
-                            let xx = mx * nx + x;
-                            if ps.contains(&(xx, yy).into()) {
-                                print!("O");
-                            } else {
-                                print!("{}", grid[(x, y)]);
+        if log_enabled!(Info) {
+            {
+                info!(
+                    "[step {step:2>}] plots={}, tiles={}",
+                    ps.len(),
+                    ps.iter().unique_by(|MetaPoint { tile, .. }| tile).count()
+                );
+            }
+
+            if [1, steps].contains(&step) {
+                // Render a 3x3 grid of the maps
+                for my in -1..2 {
+                    for y in grid.range_y().rev() {
+                        for mx in -1..2 {
+                            let tile = IVec2::new(mx, my);
+                            for x in grid.range_x() {
+                                if ps.contains(&MetaPoint {
+                                    in_tile: IVec2::new(x, y),
+                                    tile,
+                                }) {
+                                    print!("O");
+                                } else {
+                                    print!("{}", grid[(x, y)]);
+                                }
+                            }
+                            if spacing {
+                                print!(" ");
                             }
                         }
-                        print!(" ");
+                        println!();
                     }
-                    println!();
+                    if spacing {
+                        println!();
+                    }
                 }
                 println!();
             }
-            println!();
         }
     }
 
     ps.len() as i64
 }
 
+fn do_part2(steps: i64, input: &str) -> i64 {
+    use num::traits::Pow;
+
+    if steps < 20 {
+        do_part2_sim(steps, input)
+    } else {
+        let x0 = 10;
+        let x1 = 100;
+        let x0log = (x0 as f64).ln();
+        let x1log = (x1 as f64).ln();
+
+        let y0 = do_part2_sim(x0, input) as f64;
+        let y1 = do_part2_sim(x1, input) as f64;
+        let y0log = y0.ln();
+        let y1log = y1.ln();
+
+        let m = (y1log - y0log) / (x1log - x0log);
+        let b = y0log - (m * x0log);
+
+        assert_eq!(y0, (f64::exp(b) * (x0 as f64).pow(m)).round());
+        assert_eq!(y1, (f64::exp(b) * (x1 as f64).pow(m)).round());
+
+        info!("y_log = {m} * x_log + {b}");
+        info!("y = e^{b} * x^({m})");
+
+        let r = f64::exp(b) * (steps as f64 - 1.).pow(m);
+        info!("r={r}");
+
+        r as i64
+    }
+}
+
 #[aoc(day21, part2)]
 pub fn part2(input: &str) -> i64 {
-    do_part2(26_501_365, input)
+    let r = do_part2(26_501_365, input);
+
+    assert!(r > 91510463199137);
+    assert!(r > 255114563307125);
+
+    r
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    #[allow(unused_imports)]
-    use pretty_assertions::{assert_eq, assert_ne};
     use rstest::*;
     use std::time::Duration;
 
@@ -155,9 +259,8 @@ mod test {
     #[trace]
     fn check_ex_part_1(
         #[notrace]
-        #[values(part1)]
-        p: impl FnOnce(&str) -> i64,
-        #[case] steps: i64,
+        #[case]
+        steps: i64,
         #[case] expected: i64,
         #[case] input: &str,
     ) {
@@ -168,8 +271,8 @@ mod test {
     fn ms(ms: u32) -> Duration {
         Duration::from_millis(ms.into())
     }
+
     #[rstest]
-    #[case::given(6, 16, EXAMPLE_INPUT)]
     #[case::given_10(10, 50, EXAMPLE_INPUT)]
     #[case::given_50(50, 1594, EXAMPLE_INPUT)]
     #[case::given_100(100, 6536, EXAMPLE_INPUT)]
@@ -180,13 +283,30 @@ mod test {
     #[trace]
     fn check_ex_part_2(
         #[notrace]
-        #[values(part2)]
-        p: impl FnOnce(&str) -> i64,
-        #[case] steps: i64,
+        #[case]
+        steps: i64,
         #[case] expected: i64,
         #[case] input: &str,
     ) {
         let input = input.trim();
         assert_eq!(do_part2(steps, input), expected);
+    }
+
+    #[rstest]
+    #[case::given_p1(6, 16, EXAMPLE_INPUT)]
+    #[case::given_10(10, 50, EXAMPLE_INPUT)]
+    #[case::given_50(50, 1594, EXAMPLE_INPUT)]
+    #[case::given_100(100, 6536, EXAMPLE_INPUT)]
+    #[timeout(ms(1_000))]
+    #[trace]
+    fn check_ex_part_2_sim(
+        #[notrace]
+        #[case]
+        steps: i64,
+        #[case] expected: i64,
+        #[case] input: &str,
+    ) {
+        let input = input.trim();
+        assert_eq!(do_part2_sim(steps, input), expected);
     }
 }
