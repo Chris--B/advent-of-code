f4d72839c091b5e376a743b8036a7f03e5f84ab0
Chris <chrisbutler296@gmail.com>
2024-12-06 09:48:36 -0700

    2024 Day 6 Part 2, Brute forced

--- 2024/src/day06.rs
+++ 2024/src/day06.rs
@@ -48,9 +48,82 @@ pub fn part1(input: &str) -> i32 {
 }
 
 // Part2 ========================================================================
+fn loops(map: &Framebuffer<char>, start: (IVec2, Cardinal)) -> bool {
+    let mut guard = start;
+    let mut history: HashSet<(IVec2, Cardinal)> = [start].into_iter().collect();
+
+    loop {
+        let next = guard.0 + guard.1.into();
+        if history.contains(&(next, guard.1)) {
+            return true;
+        }
+
+        if let Some(c) = map.get(next.x as isize, next.y as isize) {
+            if *c == '#' || *c == 'O' {
+                guard.1 = guard.1.turn_right();
+            } else {
+                guard.0 = next;
+
+                history.insert(guard);
+            }
+        } else {
+            break;
+        }
+    }
+
+    false
+}
+
 #[aoc(day6, part2)]
 pub fn part2(input: &str) -> i32 {
-    0
+    let mut start = (IVec2::zero(), Norð);
+    let mut map = Framebuffer::parse_grid2(input, |info| {
+        if info.c == '^' {
+            start.0 = IVec2::new(info.x, info.y);
+        }
+        info.c
+    });
+
+    let start = start;
+    let mut guard = start;
+
+    // Run once to get our history
+    let mut history = HashSet::new();
+    history.insert(start.0);
+    loop {
+        let next = guard.0 + guard.1.into();
+
+        if let Some(c) = map.get_mut(next.x as isize, next.y as isize) {
+            if *c == '#' || *c == 'O' {
+                guard.1 = guard.1.turn_right();
+            } else {
+                guard.0 = next;
+                history.insert(guard.0);
+            }
+        } else {
+            break;
+        }
+    }
+
+    // And then for each spot we visisted, we'll check if an obstacle there triggers a loop.
+    let percent = 100. * (history.len() as f64) / (map.width() * map.height()) as f64;
+    println!(
+        "Checking {} locations ({percent:.1}% of map)",
+        history.len()
+    );
+
+    let mut options = 0;
+    for step in history {
+        // Add our new obstacle
+        map[step] = 'O';
+        if loops(&map, start) {
+            options += 1;
+        }
+        // And un-add it
+        map[step] = '.';
+    }
+
+    options
 }
 
 #[cfg(test)]
