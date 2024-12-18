20f3e5c34e6e8aa583bdb22709277ccd399e8716
Chris <chrisbutler296@gmail.com>
2024-12-18 13:38:45 -0700

    2024 Day 18 - what if algorithms?

--- 2024/src/day18.rs
+++ 2024/src/day18.rs
@@ -1,12 +1,6 @@
-#![allow(unused)]
-
-use indicatif::ProgressBar;
-
 use crate::prelude::*;
 
 fn steps_to(start: IVec2, end: IVec2, map: &Framebuffer<char>) -> Option<i32> {
-    let dim = map.width();
-
     let mut cost_map: Framebuffer<i32> = Framebuffer::new_matching_size(map);
     cost_map.clear(i32::MAX);
     cost_map[start] = 0;
@@ -36,6 +30,38 @@ fn steps_to(start: IVec2, end: IVec2, map: &Framebuffer<char>) -> Option<i32> {
         None
     } else {
         Some(cost_map[end])
+    }
+}
+
+fn steps_to_partial(
+    start: IVec2,
+    end: IVec2,
+    map: &Framebuffer<char>,
+    cost_map: &mut Framebuffer<i32>,
+) {
+    let mut seen = HashSet::new();
+    let mut queue = vec![start];
+    while let Some(curr) = queue.pop() {
+        if curr == end {
+            // break;
+        }
+        if seen.contains(&curr) {
+            continue;
+        }
+        seen.insert(curr);
+
+        let curr_cost = cost_map[curr];
+        for dir in Cardinal::ALL_NO_DIAG {
+            let next = curr + dir.into();
+            let next_cost = curr_cost + 1;
+
+            if let Some(&cost) = cost_map.get(next.x as _, next.y as _) {
+                if (map[next] == '.') && (next_cost <= cost) {
+                    cost_map[next] = next_cost;
+                    queue.push(next);
+                }
+            }
+        }
     }
 }
 
@@ -74,15 +100,8 @@ impl std::fmt::Display for Coord {
     }
 }
 
-#[aoc(day18, part2, bruteforce)]
-pub fn part2_bruteforce(input: &str) -> Coord {
-    println!("Running day18 part2()");
-
-    if !cfg!(test) {
-        println!("  jk, comment out this to actually run it");
-        return Coord((-1, -1));
-    }
-
+#[aoc(day18, part2)]
+pub fn part2(input: &str) -> Coord {
     let dim = if cfg!(test) { 6 } else { 70 } + 1;
 
     let mut map = Framebuffer::new(dim as u32, dim as u32);
@@ -91,64 +110,28 @@ pub fn part2_bruteforce(input: &str) -> Coord {
     let start = IVec2::new(0, dim - 1);
     let end = IVec2::new(dim - 1, 0);
 
-    let pb = ProgressBar::new(input.i64s().count() as u64 / 2);
-    for (x, y) in input.i64s().map(|n| n as i32).tuples() {
-        let y = dim - y - 1;
-        map[(x, y)] = '#';
+    // Build the map fully
+    let coords = input.i64s().map(|n| n as i32).tuples().collect_vec();
+    for &(x, y) in &coords {
+        map[(x, dim - y - 1)] = '#';
+    }
 
-        let maybe_steps = steps_to(start, end, &map);
-        pb.inc(1);
+    let mut cost_map: Framebuffer<i32> = Framebuffer::new_matching_size(&map);
+    cost_map.clear(i32::MAX);
+    cost_map[start] = 0;
 
-        if maybe_steps.is_none() {
-            pb.finish();
-            let y = dim - y - 1;
-            return Coord((x, y));
+    // Peel off the coordinates one-by-one until we find a path that works
+    for (i, &(x, y)) in coords.iter().enumerate().rev() {
+        map[(x, dim - y - 1)] = '.';
+        steps_to_partial(start, end, &map, &mut cost_map);
+
+        if cost_map[end] != i32::MAX {
+            // This means blocking coords[i] prevents a valid path.
+            return Coord(coords[i]);
         }
     }
-    pb.finish();
 
-    unreachable!()
-}
-
-#[aoc(day18, part2, rayon)]
-pub fn part2_rayon(input: &str) -> Coord {
-    use rayon::iter::{IntoParallelIterator, ParallelIterator};
-    println!("Running day18 part2_rayon()");
-
-    let dim = if cfg!(test) { 6 } else { 70 } + 1;
-
-    let mut map = Framebuffer::new(dim as u32, dim as u32);
-    map.clear('.');
-
-    let start = IVec2::new(0, dim - 1);
-    let end = IVec2::new(dim - 1, 0);
-
-    let coords: Vec<_> = input.i64s().map(|n| n as i32).tuples().collect_vec();
-    let counts: Vec<_> = (1..=coords.len()).collect_vec();
-
-    let pb = ProgressBar::new(counts.len() as u64);
-    let answer: Vec<(usize, Coord)> = counts
-        .into_par_iter()
-        .filter_map(|n| {
-            let mut map = map.clone();
-            for &(x, y) in &coords[..n] {
-                map[(x, dim - y - 1)] = '#';
-            }
-
-            let maybe_steps = steps_to(start, end, &map);
-            pb.inc(1);
-
-            if maybe_steps.is_none() {
-                Some((n, Coord(coords[n - 1])))
-            } else {
-                None
-            }
-        })
-        .collect();
-    pb.finish();
-
-    let (_, coord) = answer.into_iter().min_by_key(|&(n, _)| n).unwrap();
-    coord
+    unreachable!("We never found a path?")
 }
 
 #[cfg(test)]
@@ -207,7 +190,7 @@ mod test {
     #[trace]
     fn check_ex_part_2(
         #[notrace]
-        #[values(part2_bruteforce, part2_rayon)]
+        #[values(part2)]
         p: impl FnOnce(&str) -> Coord,
         #[case] expected: (i32, i32),
         #[case] input: &str,
