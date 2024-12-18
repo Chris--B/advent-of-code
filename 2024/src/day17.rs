e4535821679a2d5947890b170a9a0579ecb61e34
Chris <chrisbutler296@gmail.com>
2024-12-18 10:54:37 -0700

    2024 Day 17 Part 2 very bork

--- 2024/src/day17.rs
+++ 2024/src/day17.rs
@@ -6,8 +6,24 @@ const A: usize = 0;
 const B: usize = 1;
 const C: usize = 2;
 
+#[derive(Copy, Clone, Debug)]
+struct Opts {
+    break_after: Option<usize>,
+    verbose: bool,
+    jnz_verbose: bool,
+}
+impl Opts {
+    fn none() -> Self {
+        Opts {
+            break_after: None,
+            verbose: false,
+            jnz_verbose: false,
+        }
+    }
+}
+
 // Part1 ========================================================================
-fn concat(out: &[i64]) -> String {
+fn concat<T: std::fmt::Display>(out: &[T]) -> String {
     out.iter().join(",")
 }
 
@@ -120,167 +136,39 @@ pub fn part1(input: &str) -> String {
     concat(&out)
 }
 
-#[aoc(day17, part1, sim)]
-pub fn part1_sim(input: &str) -> String {
-    let mut iter = input.i64s();
-    let mut a = iter.next().unwrap();
-    let mut b = iter.next().unwrap();
-    let mut c = iter.next().unwrap();
+#[aoc(day17, part1, native)]
+pub fn part1_native(input: &str) -> String {
+    let a = input.i64s().next().unwrap();
 
-    let program: Vec<_> = iter.collect_vec();
-
-    concat(&do_sim([a, b, c], &program, Opts::none()))
+    let mut opts = Opts::none();
+    opts.verbose = true;
+    concat(&do_sim__2_4__1_1__7_5__1_5__4_0__5_5__0_3__3_0(a))
 }
 
-fn opcode_str(op: i64) -> &'static str {
-    match op {
-        0 => "adv",
-        1 => "bxl",
-        2 => "bst",
-        3 => "jnz",
-        4 => "bxc",
-        5 => "out",
-        6 => "bdv",
-        7 => "cdv",
-        _ => unreachable!("{op} is not a valid opcode"),
-    }
-}
+#[allow(non_snake_case)]
+fn do_sim__2_4__1_1__7_5__1_5__4_0__5_5__0_3__3_0(mut a: i64) -> SmallVec<[u8; 12]> {
+    // println!("regs[A] % 64 == {a:2} 0b{a:06b}", a = a & 0b111_111);
+    let mut out: SmallVec<[u8; 12]> = smallvec::smallvec![];
 
-struct Opts {
-    break_after: Option<usize>,
-    verbose: bool,
-}
-impl Opts {
-    fn none() -> Self {
-        Opts {
-            break_after: None,
-            verbose: false,
+    loop {
+        // println!("regs[A] % 64 == {a:2} 0b{a:06b}", a = a & 0b111_111);
+
+        let b = ((a ^ 0b01) % 8) as u8;
+        let c = ((a >> b) % 8) as u8;
+
+        out.push(b ^ c);
+
+        a >>= 3;
+        if a == 0 {
+            break;
         }
     }
-}
 
-fn do_sim(mut regs: [i64; 3], program: &[i64], opts: Opts) -> Vec<i64> {
-    let mut ip = 0;
-    let mut out: Vec<i64> = vec![];
-
-    while ip + 1 < program.len() {
-        let opcode = program[ip];
-        let lit = program[ip + 1];
-
-        let combo = match lit {
-            0..=3 => lit,
-            4 => regs[A],
-            5 => regs[B],
-            6 => regs[C],
-            // reserved:
-            7 => {
-                if ![0, 2, 5, 6, 7].contains(&opcode) {
-                    unreachable!("Reserved combo operand used");
-                }
-                lit
-            }
-            _ => unreachable!("Illegal combo operand"),
-        };
-        let combo_str = match lit {
-            0 => "combo.0",
-            1 => "combo.1",
-            2 => "combo.2",
-            3 => "combo.3",
-            4 => "combo.A",
-            5 => "combo.B",
-            6 => "combo.C",
-            7 => "combo.7",
-            _ => unreachable!("Illegal combo operand"),
-        };
-
-        if opts.verbose {
-            println!("{}", opcode_str(opcode));
-        }
-        match opcode {
-            // The adv instruction (opcode 0) performs division.
-            0 => {
-                if opts.verbose {
-                    println!("  + A = A / (1 << {combo_str}");
-                }
-                regs[A] /= (1 << combo);
-            }
-
-            // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
-            1 => {
-                if opts.verbose {
-                    println!("  + B = B ^ {lit}");
-                }
-                regs[B] ^= lit;
-            }
-
-            // The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
-            2 => {
-                if opts.verbose {
-                    println!("  + B = {combo_str} % 8 (={})", combo % 8);
-                }
-                regs[B] = combo % 8;
-            }
-
-            // The jnz instruction (opcode 3) does nothing if the A register is 0.
-            3 => {
-                if opts.verbose {
-                    println!("  + if A != 0 goto {lit}");
-                }
-                if regs[A] != 0 {
-                    ip = lit as usize;
-                    continue;
-                }
-            }
-
-            // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B.
-            4 => {
-                if opts.verbose {
-                    println!("  + B = B ^ C");
-                }
-                regs[B] ^= regs[C];
-            }
-
-            // The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
-            5 => {
-                if opts.verbose {
-                    println!("  + out {combo_str} % 8 (={})", combo % 8);
-                }
-                out.push(combo % 8);
-                if Some(out.len()) == opts.break_after {
-                    if opts.verbose {
-                        println!("DEBUG: Breaking after {:?} out(s)!", opts.break_after);
-                    }
-                    break;
-                }
-            }
-
-            // The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register.
-            6 => {
-                if opts.verbose {
-                    println!("  + B = A / (1 << {combo})");
-                }
-                regs[B] = regs[A] / (1 << combo);
-            }
-
-            // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register.
-            7 => {
-                if opts.verbose {
-                    println!("  + C = A / (1 << {combo})");
-                }
-                regs[C] = regs[A] / (1 << combo);
-            }
-
-            //
-            _ => unreachable!("Illegal opcode: {opcode} (ip={ip})"),
-        }
-
-        ip += 2;
+    for o in &mut out {
+        *o ^= 5;
     }
 
-    if opts.verbose {
-        println!("[{ip:>2}] {regs:?}");
-        println!("out={out:?}");
-    }
+    // println!();
 
     out
 }
@@ -296,30 +184,79 @@ fn p8(n: u8) -> i64 {
 
 // #[aoc(day17, part2)]
 pub fn part2(input: &str) -> i64 {
-    let mut iter = input.i64s();
-
-    let _ = iter.next().unwrap();
-    let b = iter.next().unwrap();
-    let c = iter.next().unwrap();
-
-    let program: Vec<_> = iter.collect_vec();
+    let mut iter = input.i64s().skip(3).map(|p| p as u8);
+    let mut program: Vec<_> = iter.collect_vec();
     println!("program={program:?}");
+    assert_eq!(program, [2, 4, 1, 1, 7, 5, 1, 5, 4, 0, 5, 5, 0, 3, 3, 0]);
 
-    // do_sim([0, b, c], &program, Some(1));
+    program.reverse();
 
-    // // discover a?
-    // let mut a = 4;
-    // let mut b = 0;
-    // let mut c = 0;
+    for a in 0..64 {
+        let outs = do_sim__2_4__1_1__7_5__1_5__4_0__5_5__0_3__3_0(a);
+        if program.starts_with(&outs) {
+            println!("[{a:>4} {a:014b}] outs={outs:?}");
+        }
+    }
 
-    // b = a % 8;
-    // b ^= 0b100;
-    // b ^= (a / 2);
+    // Check all 1-digit outs
+    for a in 0..8 {
+        let outs = do_sim__2_4__1_1__7_5__1_5__4_0__5_5__0_3__3_0(a);
+        println!("  + a={a} ({a:03b}) -> {outs:?}");
+    }
 
-    // let out = b % 8;
+    let mut a: i64 = 4;
+    let mut t: u8 = 3;
+    for _ in 0..5 {
+        {
+            let mut candidates = [0; 8];
 
-    // assert_eq!(out, 2);
+            // Find 3-bit b and c s.t.:
+            //      b ^ c ^ 5 == t mod 8
+            for b in 0..8 {
+                let c = t ^ b ^ 5;
 
+                // let b = (a ^ 1);
+                let ab = b ^ 1;
+                // let c = (a >> b);
+                let ac = c << b;
+
+                let a = ab | ac;
+                candidates[b as usize] = a;
+
+                // println!("b =0b{b:06b}");
+                // println!("c =0b{c:06b}");
+                // println!("ab=0b{ab:06b}");
+                // println!("ac=0b{ac:06b}");
+                // println!("a =0b{a:06b} ({a})",);
+                // println!();
+            }
+            println!("candidates={candidates:?}");
+            candidates.sort();
+            a |= (candidates[0] as i64);
+            a *= 8;
+        }
+
+        let outs = do_sim__2_4__1_1__7_5__1_5__4_0__5_5__0_3__3_0(a);
+        println!("  + a={a} ({a:03b})");
+        println!("    -> {outs:?}");
+        println!("    program={program:?}");
+        println!();
+        t = program[outs.len()];
+    }
+
+    // for a in 0..64 * 64 * 64 {
+    //     let outs = do_sim__2_4__1_1__7_5__1_5__4_0__5_5__0_3__3_0(a);
+    //     if program.starts_with(&outs) {
+    //         print!("[{a:>4} {a:014b}] outs={outs:?}");
+    //         println!(" *");
+    //     } else {
+    //         // println!();
+    //     }
+    // }
+    // println!("program={program:?}");
+    // println!("outs={outs:?}");
+
+    unreachable!();
     /*
         Import Observations:
             1. B and C are 0, A is the only initialized register
@@ -387,87 +324,146 @@ pub fn part2(input: &str) -> i64 {
                     [117440] outs=[0, 3, 5, 4, 3, 0]
             12. I think I need to read the opcodes more closely to understand how they pick outputs...
     */
+}
 
-    assert_eq!([b, c], [0, 0]);
-
-    let mut prev = vec![1];
-    for a in 0..=(8 * 9) {
-        let outs = do_sim([a, 0, 0], &program, Opts::none());
-        if outs.len() != prev.len() {
-            println!();
-        }
-
-        if program.starts_with(&outs) {
-            println!("{a:>2} -> {outs:?} ****");
-        } else {
-            println!("{a:>2} -> {outs:?}");
-        }
-        prev = outs;
+fn opcode_str(op: i64) -> &'static str {
+    match op {
+        0 => "adv",
+        1 => "bxl",
+        2 => "bst",
+        3 => "jnz",
+        4 => "bxc",
+        5 => "out",
+        6 => "bdv",
+        7 => "cdv",
+        _ => unreachable!("{op} is not a valid opcode"),
     }
-    println!();
+}
 
-    let max = 117440;
-    println!("Looking for early matches... (0..={max})");
-    {
-        let mut a: u64 = 0;
-        while a <= max {
-            let outs = do_sim([a as i64, 0, 0], &program, Opts::none());
-            if program.starts_with(&outs) {
-                println!("[{a:>6}] outs={outs:?}");
+fn do_sim(mut regs: [i64; 3], program: &[i64], opts: Opts) -> Vec<u8> {
+    let mut ip = 0;
+    let mut out = vec![];
+
+    while ip + 1 < program.len() {
+        let opcode = program[ip];
+        let lit = program[ip + 1];
+
+        let combo = match lit {
+            0..=3 => lit,
+            4 => regs[A],
+            5 => regs[B],
+            6 => regs[C],
+            // reserved:
+            7 => {
+                if ![0, 2, 5, 6, 7].contains(&opcode) {
+                    unreachable!("Reserved combo operand used");
+                }
+                lit
             }
-            a += 1;
+            _ => unreachable!("Illegal combo operand"),
+        };
+        let combo_str = match lit {
+            0 => "combo.0",
+            1 => "combo.1",
+            2 => "combo.2",
+            3 => "combo.3",
+            4 => "combo.A",
+            5 => "combo.B",
+            6 => "combo.C",
+            7 => "combo.7",
+            _ => unreachable!("Illegal combo operand"),
+        };
+
+        if opts.verbose {
+            println!("{}", opcode_str(opcode));
         }
-    }
-    println!();
+        match opcode {
+            // The adv instruction (opcode 0) performs division.
+            0 => {
+                if opts.verbose {
+                    println!("  + A = A / (1 << {combo_str}");
+                }
+                regs[A] /= (1 << combo);
+            }
 
-    if cfg!(test) {
-        println!("Interesting values...");
-        for a in [
-            6,  //
-            14, //
-            332, //
-                // 23948989, //
-                // 23949245, //
-        ] {
-            let outs = do_sim([a, 0, 0], &program, Opts::none());
-            println!("[{a:>4}] outs={outs:?}");
+            // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
+            1 => {
+                if opts.verbose {
+                    println!("  + B = B ^ {lit}");
+                }
+                regs[B] ^= lit;
+            }
+
+            // The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+            2 => {
+                if opts.verbose {
+                    println!("  + B = {combo_str} % 8 (={})", combo % 8);
+                }
+                regs[B] = combo % 8;
+            }
+
+            // The jnz instruction (opcode 3) does nothing if the A register is 0.
+            3 => {
+                if opts.verbose || opts.jnz_verbose {
+                    println!("  + if A ({A}) != 0 goto {lit}", A = regs[A]);
+                }
+                if regs[A] != 0 {
+                    ip = lit as usize;
+                    continue;
+                }
+            }
+
+            // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B.
+            4 => {
+                if opts.verbose {
+                    println!("  + B = B ^ C");
+                }
+                regs[B] ^= regs[C];
+            }
+
+            // The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
+            5 => {
+                if opts.verbose {
+                    println!("  + out {combo_str} % 8 (={})", combo % 8);
+                }
+                out.push((combo % 8) as u8);
+                if Some(out.len()) == opts.break_after {
+                    if opts.verbose {
+                        println!("DEBUG: Breaking after {:?} out(s)!", opts.break_after);
+                    }
+                    break;
+                }
+            }
+
+            // The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register.
+            6 => {
+                if opts.verbose {
+                    println!("  + B = A / (1 << {combo})");
+                }
+                regs[B] = regs[A] / (1 << combo);
+            }
+
+            // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register.
+            7 => {
+                if opts.verbose {
+                    println!("  + C = A / (1 << {combo})");
+                }
+                regs[C] = regs[A] / (1 << combo);
+            }
+
+            //
+            _ => unreachable!("Illegal opcode: {opcode} (ip={ip})"),
         }
-        println!();
+
+        ip += 2;
     }
 
-    // let mut cache = (0..8)
-    //     .map(|a| do_sim([a, 0, 0], &program, Opts::none())[0])
-    //     .collect_vec();
+    if opts.verbose {
+        println!("[{ip:>2}] {regs:?}");
+        println!("out={out:?}");
+    }
 
-    // let mut queue = (0..=8).collect_vec();
-    // let mut seen = HashSet::new();
-    // while let Some(a) = queue.pop() {
-    //     // println!("[{:>4}] a = {a}", queue.len());
-    //     if a > 1000 {
-    //         continue;
-    //     }
-    //     if seen.contains(&a) {
-    //         continue;
-    //     }
-
-    //     seen.insert(a);
-    //     let outs = do_sim([a, 0, 0], &program, Opts::none());
-    //     if outs == program {
-    //         return a;
-    //     }
-
-    //     if program.starts_with(&outs) {
-    //         let next = program[outs.len()];
-    //         println!("👀 a={a} outs={outs:?} (need {next})");
-    //         for (i, &c) in cache.iter().enumerate() {
-    //             if c == next {
-    //                 println!("i={i}");
-    //             }
-    //         }
-    //     }
-    // }
-
-    unreachable!()
+    out
 }
 
 #[cfg(test)]
@@ -535,6 +531,20 @@ Register C: 0
 Program: 0,3,5,4,3,0
 ";
 
+    #[test]
+    fn check_jit_sim() {
+        let opts = Opts::none();
+        for a in 0..10_000 {
+            let out_jit = do_sim__2_4__1_1__7_5__1_5__4_0__5_5__0_3__3_0(a);
+            let out = do_sim(
+                [a, 0, 0],
+                &[2, 4, 1, 1, 7, 5, 1, 5, 4, 0, 5, 5, 0, 3, 3, 0],
+                opts,
+            );
+            assert_eq!(out.as_slice(), out_jit.as_slice());
+        }
+    }
+
     #[rstest]
     #[case::given(concat(&[4,6,3,5,6,3,5,2,1,0]), EXAMPLE_INPUT)]
     #[case::mini_ex_2(concat(&[0,1,2]), MINI_EX_2)]
@@ -543,7 +553,7 @@ Program: 0,3,5,4,3,0
     #[trace]
     fn check_ex_part_1(
         #[notrace]
-        #[values(part1, part1_sim)]
+        #[values(part1)]
         p: impl FnOnce(&str) -> String,
         #[case] expected: String,
         #[case] input: &str,
@@ -562,20 +572,21 @@ Register C: 0
 Program: 0,3,5,4,3,0
 ";
 
-    #[rstest]
-    #[case::given_117440(117440, EXAMPLE_INPUT_P2)]
-    #[timeout(Duration::from_millis(100))]
-    #[trace]
-    fn check_ex_part_2(
-        #[notrace]
-        #[values(part2)]
-        p: impl FnOnce(&str) -> i64,
-        #[case] expected: i64,
-        #[case] input: &str,
-    ) {
-        init_logging();
+    // #[rstest]
+    // #[ignore]
+    // #[case::given_117440(117440, EXAMPLE_INPUT_P2)]
+    // #[timeout(Duration::from_millis(100))]
+    // #[trace]
+    // fn check_ex_part_2(
+    //     #[notrace]
+    //     #[values(part2)]
+    //     p: impl FnOnce(&str) -> i64,
+    //     #[case] expected: i64,
+    //     #[case] input: &str,
+    // ) {
+    //     init_logging();
 
-        let input = input.trim();
-        assert_eq!(p(input), expected);
-    }
+    //     let input = input.trim();
+    //     assert_eq!(p(input), expected);
+    // }
 }
