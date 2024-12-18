b1f2faa82860a234efa6ac5e17fe700b9d622161
Chris <chrisbutler296@gmail.com>
2024-12-18 11:08:10 -0700

    Add new_day.sh helper

---
+++ new_day.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+day=$1
+echo "New file for day $day"
+
+set -ex
+# No dirty state pls
+test -z "$(git status --porcelain)"
+
+cp src/day{00,$day}.rs
+git add -N src/day$day.rs
+
+cargo aoc input --day $day
+gsed -i "s%// pub mod day$day;%pub mod day$day;%g" src/lib.rs
+gsed -i 's/dayN/day18/g' src/day$day.rs
+
+cargo aoc input --day $day
+cargo aoc --day $day
