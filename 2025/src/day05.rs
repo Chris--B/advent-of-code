1f2174286e28d00040f07bde04afcd24c772595d
Chris <chrisbutler296@gmail.com>
2025-12-04 23:35:59 -0700

    2025 Day 5

---
+++ 2025/src/day05.rs
@@ -0,0 +1,143 @@
+#![allow(unused)]
+
+use crate::prelude::*;
+
+// Part1 ========================================================================
+#[aoc(day5, part1)]
+pub fn part1(input: &str) -> i64 {
+    let ranges_count = input.lines().take_while(|l| l.trim() != "").count();
+
+    let mut ranges: Vec<(i64, i64)> = input
+        .i64s()
+        .tuples()
+        .map(|(a, b)| (a, -b))
+        .take(ranges_count)
+        .collect();
+    ranges.sort();
+
+    let ids: Vec<i64> = input.i64s().skip(2 * ranges_count).collect();
+
+    if cfg!(test) {
+        println!("{:>8}={:?}", "ranges", ranges);
+        println!("{:>8}={:?}", "ids", ids);
+    }
+    ids.into_iter()
+        .filter(|id| {
+            for (a, b) in &ranges {
+                if (a..=b).contains(&id) {
+                    return true;
+                }
+            }
+            false
+        })
+        .count() as i64
+}
+
+// Part2 ========================================================================
+#[aoc(day5, part2)]
+pub fn part2(input: &str) -> i64 {
+    let ranges_count = input.lines().take_while(|l| l.trim() != "").count();
+
+    let mut ranges: Vec<_> = input
+        .i64s()
+        .tuples()
+        .map(|(a, b)| (a, -b))
+        .take(ranges_count)
+        .collect();
+
+    ranges.sort();
+
+    if cfg!(test) {
+        println!("PRE  ranges: {ranges:?}");
+    }
+
+    let mut i = 0;
+    while i + 1 < ranges.len() {
+        if ranges[i].1 < ranges[i + 1].0 {
+            // no overlap, continue
+            i += 1;
+        } else {
+            let a = ranges[i].0;
+            let b = i64::max(ranges[i].1, ranges[i + 1].1);
+
+            ranges[i] = (a, b);
+            ranges.remove(i + 1);
+        }
+    }
+
+    if cfg!(test) {
+        println!("POST ranges: {ranges:?}");
+    }
+
+    ranges.into_iter().map(|(a, b)| b - a + 1).sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[allow(unused_imports)]
+    use pretty_assertions::{assert_eq, assert_ne};
+    use rstest::*;
+
+    const EXAMPLE_INPUT: &str = r"
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
+
+    const EQUAL_BOUNDS: &str = r"
+10-20
+20-30
+
+";
+
+    const SUPERSET_BOUNDS: &str = r"
+1-100
+10-20
+20-30
+
+";
+
+    #[rstest]
+    #[case::given(3, EXAMPLE_INPUT)]
+    #[trace]
+    fn check_ex_part_1(
+        #[notrace]
+        #[values(part1)]
+        p: impl FnOnce(&str) -> i64,
+        #[case] expected: i64,
+        #[case] input: &str,
+    ) {
+        init_logging();
+
+        let input = input.trim();
+        assert_eq!(p(input), expected);
+    }
+
+    #[rstest]
+    #[case::given(14, EXAMPLE_INPUT)]
+    #[case::equal_bounds(21, EQUAL_BOUNDS)]
+    #[case::superset(100, SUPERSET_BOUNDS)]
+    #[timeout(Duration::from_millis(10))]
+    #[trace]
+    fn check_ex_part_2(
+        #[notrace]
+        #[values(part2)]
+        p: impl FnOnce(&str) -> i64,
+        #[case] expected: i64,
+        #[case] input: &str,
+    ) {
+        init_logging();
+
+        let input = input.trim();
+        assert_eq!(p(input), expected);
+    }
+}
